e68162ccb34f3a8d84da35ce192a812c767bdcdf
baldurk <baldurk@baldurk.org>
2014-08-25 12:30:37 +0100

    Bump version numbers to 0.21, as we're closer to that than 0.20 now

--- renderdocui/Properties/AssemblyInfo.cs
+++ renderdocui/Properties/AssemblyInfo.cs
@@ -57,8 +57,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.20.0.0")]
-[assembly: AssemblyFileVersion("0.20.0.0")]
+[assembly: AssemblyVersion("0.21.0.0")]
+[assembly: AssemblyFileVersion("0.21.0.0")]
 
 // this can be replaced with the git hash of the commit being built from e.g. in a script
 [assembly: AssemblyInformationalVersion("NO_GIT_COMMIT_HASH_DEFINED")]
