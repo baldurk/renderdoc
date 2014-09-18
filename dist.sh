9c23d67c30e80a24bd55d0d6c74b51fe00289279
baldurk <baldurk@baldurk.org>
2014-09-18 19:02:28 +0100

    Pack-in d3dcompiler_47.dll with distribution. Refs #81

--- dist.sh
+++ dist.sh
@@ -22,6 +22,10 @@ mkdir -p dist/Release{32,64}
 cp -R x64/Release/* dist/Release64/
 cp -R Win32/Release/* dist/Release32/
 
+# Copy in d3dcompiler from windows kit 8.1
+cp /c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/Redist/D3D/x64/d3dcompiler_47.dll dist/Release64/
+cp /c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/Redist/D3D/x86/d3dcompiler_47.dll dist/Release32/
+
 # Copy associated files that should be included with the distribution
 cp LICENSE.md Documentation/*.chm dist/Release64/
 cp LICENSE.md Documentation/*.chm dist/Release32/
