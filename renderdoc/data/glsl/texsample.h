a282b68c58ad0d5f9f1772aef1ba70567120776c
baldurk <baldurk@baldurk.org>
2015-02-09 11:48:31 +0000

    Flip UV sample vertically for cubemap +ve X direction

--- renderdoc/data/glsl/texsample.h
+++ renderdoc/data/glsl/texsample.h
@@ -24,11 +24,12 @@
 
 vec3 CalcCubeCoord(vec2 uv, int face)
 {
+	// From table 8.19 in GL4.5 spec
 	// Map UVs to [-0.5, 0.5] and rotate
 	uv -= vec2(0.5);
 	vec3 coord;
 	if (face == CUBEMAP_FACE_POS_X)
-		coord = vec3(0.5, uv.y, -uv.x);
+		coord = vec3(0.5, -uv.y, -uv.x);
 	else if (face == CUBEMAP_FACE_NEG_X)
 		coord = vec3(-0.5, -uv.y, uv.x);
 	else if (face == CUBEMAP_FACE_POS_Y)
