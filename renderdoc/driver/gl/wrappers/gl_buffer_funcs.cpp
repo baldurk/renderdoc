bf36b775b1a5203bbdae4d414d5f199605586158
baldurk <baldurk@baldurk.org>
2015-02-13 20:49:41 +0000

    Make sure to pass through flush if we really mapped underlying buffer

--- renderdoc/driver/gl/wrappers/gl_buffer_funcs.cpp
+++ renderdoc/driver/gl/wrappers/gl_buffer_funcs.cpp
@@ -1768,7 +1768,10 @@ void WrappedOpenGL::glFlushMappedNamedBufferRangeEXT(GLuint buffer, GLintptr off
 
 	// note that we only want to flush the range with GL if we've actually
 	// mapped it. Otherwise the map is 'virtual' and just pointing to our backing store data
-	if(record && record->Map.status == GLResourceRecord::Mapped_Write_Real)
+	if(record &&
+		  (record->Map.status == GLResourceRecord::Mapped_Write_Real ||
+			 record->Map.status == GLResourceRecord::Mapped_Ignore_Real)
+		)
 		m_Real.glFlushMappedNamedBufferRangeEXT(buffer, offset, length);
 
 	if(m_State == WRITING_CAPFRAME)
