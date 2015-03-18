26f2d0fbb80c147b91c50785e3cb39318bb06ef9
baldurk <baldurk@baldurk.org>
2015-03-18 21:26:51 +0000

    fix linux preprocessor macro use in API header files

--- renderdoc/api/replay/renderdoc_replay.h
+++ renderdoc/api/replay/renderdoc_replay.h
@@ -41,7 +41,7 @@ typedef uint32_t bool32;
 #endif
 #define RENDERDOC_CC __cdecl
 
-#elif defined(LINUX)
+#elif defined(__linux__)
 
 #ifdef RENDERDOC_EXPORTS
 #define RENDERDOC_API __attribute__ ((visibility ("default")))
