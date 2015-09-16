2a043d6125b0548976c27010e6a7056091f2dbd1
baldurk <baldurk@baldurk.org>
2015-09-16 11:54:14 +0200

    Implement getting backbuffer screenshot on capture

--- renderdoc/driver/vulkan/vk_core.cpp
+++ renderdoc/driver/vulkan/vk_core.cpp
@@ -25,6 +25,10 @@
 #include "vk_core.h"
 #include "serialise/string_utils.h"
 
+#include "maths/formatpacking.h"
+
+#include "jpeg-compressor/jpge.h"
+
 // VKTODOLOW dirty buffers should propagate through to their memory somehow
 // images can be separately dirty since we can't just copy their memory
 // (tiling could be different)
@@ -6742,12 +6746,258 @@ VkResult WrappedVulkan::vkQueuePresentWSI(
 			// VKTODOLOW handle present info pNext
 			RDCASSERT(pPresentInfo->pNext == NULL);
 
-			VkImage backbuffer = m_SwapChainInfo[swapid].images[pPresentInfo->imageIndices[0]].im;
+			const SwapInfo &swapInfo = m_SwapChainInfo[swapid];
+
+			VkImage backbuffer = swapInfo.images[pPresentInfo->imageIndices[0]].im;
 
 			EndCaptureFrame(backbuffer);
 			FinishCapture();
 
-			Serialiser *m_pFileSerialiser = RenderDoc::Inst().OpenWriteSerialiser(m_FrameCounter, &m_InitParams, NULL, 0, 0, 0);
+			byte *thpixels = NULL;
+			uint32_t thwidth = 0;
+			uint32_t thheight = 0;
+
+			// gather backbuffer screenshot
+			const int32_t maxSize = 1024;
+
+			// VKTODOLOW split this out properly into begin/end frame capture
+			if(1)//if(wnd)
+			{
+				VkDevice dev = GetDev();
+				VkQueue q = GetQ();
+				VkCmdBuffer cmd = GetCmd();
+
+				const VkLayerDispatchTable *vt = device_dispatch_table(dev);
+
+				// VKTODOLOW idle all devices? or just the device for this queue?
+				vt->DeviceWaitIdle(dev);
+
+				VkImage readbackIm = VK_NULL_HANDLE;
+				VkDeviceMemory readbackMem = VK_NULL_HANDLE;
+
+				VkResult vkr = VK_SUCCESS;
+
+				// create identical image
+				VkImageCreateInfo imInfo = {
+					/*.sType =*/ VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+					/*.pNext =*/ NULL,
+					/*.imageType =*/ VK_IMAGE_TYPE_2D,
+					/*.format =*/ swapInfo.format,
+					/*.extent =*/ { swapInfo.extent.width, swapInfo.extent.height, 1 },
+					/*.mipLevels =*/ 1,
+					/*.arraySize =*/ 1,
+					/*.samples =*/ 1,
+					/*.tiling =*/ VK_IMAGE_TILING_LINEAR,
+					/*.usage =*/
+					VK_IMAGE_USAGE_TRANSFER_DESTINATION_BIT,
+					/*.flags =*/ 0,
+				};
+				vt->CreateImage(dev, &imInfo, &readbackIm);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				VkMemoryRequirements mrq;
+				vkr = vt->GetImageMemoryRequirements(dev, readbackIm, &mrq);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				VkImageSubresource subr = { VK_IMAGE_ASPECT_COLOR, 0, 0 };
+				VkSubresourceLayout layout = { 0 };
+				vt->GetImageSubresourceLayout(dev, readbackIm, &subr, &layout);
+
+				// allocate readback memory
+				VkMemoryAllocInfo allocInfo = {
+					VK_STRUCTURE_TYPE_MEMORY_ALLOC_INFO, NULL,
+					mrq.size, GetReadbackMemoryIndex(mrq.memoryTypeBits),
+				};
+
+				vkr = vt->AllocMemory(dev, &allocInfo, &readbackMem);
+				RDCASSERT(vkr == VK_SUCCESS);
+				vkr = vt->BindImageMemory(dev, readbackIm, readbackMem, 0);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				VkCmdBufferBeginInfo beginInfo = { VK_STRUCTURE_TYPE_CMD_BUFFER_BEGIN_INFO, NULL, VK_CMD_BUFFER_OPTIMIZE_SMALL_BATCH_BIT | VK_CMD_BUFFER_OPTIMIZE_ONE_TIME_SUBMIT_BIT };
+
+				// do image copy
+				vkr = vt->ResetCommandBuffer(cmd, 0);
+				RDCASSERT(vkr == VK_SUCCESS);
+				vkr = vt->BeginCommandBuffer(cmd, &beginInfo);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				VkImageCopy cpy = {
+					subr,	{ 0, 0, 0 },
+					subr,	{ 0, 0, 0 },
+					{ imInfo.extent.width, imInfo.extent.height, 1 },
+				};
+
+				// VKTODOLOW back buffer must be in this layout right?
+				VkImageMemoryBarrier bbTrans = {
+					VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER, NULL,
+					0, 0, VK_IMAGE_LAYOUT_PRESENT_SOURCE_WSI, VK_IMAGE_LAYOUT_TRANSFER_SOURCE_OPTIMAL,
+					0, 0, backbuffer,
+					{ VK_IMAGE_ASPECT_COLOR, 0, 1, 0, 1 } };
+
+				VkImageMemoryBarrier readTrans = {
+					VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER, NULL,
+					0, 0, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DESTINATION_OPTIMAL,
+					0, 0, readbackIm,
+					{ VK_IMAGE_ASPECT_COLOR, 0, 1, 0, 1 } };
+
+				VkImageMemoryBarrier *barriers[] = {
+					&bbTrans,
+					&readTrans,
+				};
+
+				vt->CmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, false, 2, (void **)barriers);
+
+				vt->CmdCopyImage(cmd, backbuffer, VK_IMAGE_LAYOUT_TRANSFER_SOURCE_OPTIMAL, readbackIm, VK_IMAGE_LAYOUT_TRANSFER_DESTINATION_OPTIMAL, 1, &cpy);
+
+				// transition backbuffer back
+				std::swap(bbTrans.oldLayout, bbTrans.newLayout);
+
+				// VKTODOLOW find out correct image layout for reading back
+				readTrans.oldLayout = readTrans.newLayout;
+				readTrans.newLayout = VK_IMAGE_LAYOUT_GENERAL;
+
+				vt->CmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, false, 2, (void **)barriers);
+
+				vkr = vt->EndCommandBuffer(cmd);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				vkr = vt->QueueSubmit(q, 1, &cmd, VK_NULL_HANDLE);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				// wait queue idle
+				vt->QueueWaitIdle(q);
+
+				// map memory and readback
+				byte *pData = NULL;
+				vkr = vt->MapMemory(dev, readbackMem, 0, 0, 0, (void **)&pData);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				RDCASSERT(pData != NULL);
+
+				// point sample info into raw buffer
+				{
+					ResourceFormat fmt = MakeResourceFormat(imInfo.format);
+
+					byte *data = (byte *)pData;
+
+					data += layout.offset;
+
+					float widthf = float(imInfo.extent.width);
+					float heightf = float(imInfo.extent.height);
+
+					float aspect = widthf/heightf;
+
+					thwidth = RDCMIN(maxSize, imInfo.extent.width);
+					thwidth &= ~0x7; // align down to multiple of 8
+					thheight = uint32_t(float(thwidth)/aspect);
+
+					thpixels = new byte[3*thwidth*thheight];
+
+					uint32_t stride = fmt.compByteWidth*fmt.compCount;
+
+					bool buf1010102 = false;
+					bool bufBGRA = false;
+
+					if(fmt.special && fmt.specialFormat == eSpecial_R10G10B10A2)
+					{
+						stride = 4;
+						buf1010102 = true;
+					}
+					if(fmt.special && fmt.specialFormat == eSpecial_B8G8R8A8)
+					{
+						stride = 4;
+						bufBGRA = true;
+					}
+
+					byte *dst = thpixels;
+
+					for(uint32_t y=0; y < thheight; y++)
+					{
+						for(uint32_t x=0; x < thwidth; x++)
+						{
+							float xf = float(x)/float(thwidth);
+							float yf = float(y)/float(thheight);
+
+							byte *src = &data[ stride*uint32_t(xf*widthf) + layout.rowPitch*uint32_t(yf*heightf) ];
+
+							if(buf1010102)
+							{
+								uint32_t *src1010102 = (uint32_t *)src;
+								Vec4f unorm = ConvertFromR10G10B10A2(*src1010102);
+								dst[0] = (byte)(unorm.x*255.0f);
+								dst[1] = (byte)(unorm.y*255.0f);
+								dst[2] = (byte)(unorm.z*255.0f);
+							}
+							else if(bufBGRA)
+							{
+								dst[0] = src[2];
+								dst[1] = src[1];
+								dst[2] = src[0];
+							}
+							else if(fmt.compByteWidth == 2) // R16G16B16A16 backbuffer
+							{
+								uint16_t *src16 = (uint16_t *)src;
+
+								float linearR = RDCCLAMP(ConvertFromHalf(src16[0]), 0.0f, 1.0f);
+								float linearG = RDCCLAMP(ConvertFromHalf(src16[1]), 0.0f, 1.0f);
+								float linearB = RDCCLAMP(ConvertFromHalf(src16[2]), 0.0f, 1.0f);
+
+								if(linearR < 0.0031308f) dst[0] = byte(255.0f*(12.92f * linearR));
+								else                     dst[0] = byte(255.0f*(1.055f * powf(linearR, 1.0f/2.4f) - 0.055f));
+
+								if(linearG < 0.0031308f) dst[1] = byte(255.0f*(12.92f * linearG));
+								else                     dst[1] = byte(255.0f*(1.055f * powf(linearG, 1.0f/2.4f) - 0.055f));
+
+								if(linearB < 0.0031308f) dst[2] = byte(255.0f*(12.92f * linearB));
+								else                     dst[2] = byte(255.0f*(1.055f * powf(linearB, 1.0f/2.4f) - 0.055f));
+							}
+							else
+							{
+								dst[0] = src[0];
+								dst[1] = src[1];
+								dst[2] = src[2];
+							}
+
+							dst += 3;
+						}
+					}
+				}
+
+				vkr = vt->UnmapMemory(dev, readbackMem);
+				RDCASSERT(vkr == VK_SUCCESS);
+
+				// delete all
+				vkr = vt->DestroyImage(dev, readbackIm);
+				RDCASSERT(vkr == VK_SUCCESS);
+				vkr = vt->FreeMemory(dev, readbackMem);
+				RDCASSERT(vkr == VK_SUCCESS);
+			}
+
+			byte *jpgbuf = NULL;
+			int len = thwidth*thheight;
+
+			// VKTODOLOW split this out properly into begin/end frame capture
+			if(1)//if(wnd)
+			{
+				jpgbuf = new byte[len];
+
+				jpge::params p;
+
+				p.m_quality = 40;
+
+				bool success = jpge::compress_image_to_jpeg_file_in_memory(jpgbuf, len, thwidth, thheight, 3, thpixels, p);
+
+				if(!success)
+				{
+					RDCERR("Failed to compress to jpg");
+					SAFE_DELETE_ARRAY(jpgbuf);
+					thwidth = 0;
+					thheight = 0;
+				}
+			}
+
+			Serialiser *m_pFileSerialiser = RenderDoc::Inst().OpenWriteSerialiser(m_FrameCounter, &m_InitParams, jpgbuf, len, thwidth, thheight);
 
 			{
 				SCOPED_SERIALISE_CONTEXT(DEVICE_INIT);
