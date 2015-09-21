fe414dfc9cb9c09c230d5c45ff5bc85670cb456c
baldurk <baldurk@baldurk.org>
2015-09-21 18:58:06 +0200

    Only reset debug text on indent level 0

--- renderdoc/serialise/serialiser.cpp
+++ renderdoc/serialise/serialiser.cpp
@@ -1522,8 +1522,11 @@ uint32_t Serialiser::PushContext(const char *name, uint32_t chunkIdx, bool small
 	}
 	else
 	{
-		// reset debug text
-		m_DebugText = "";
+		if(m_Indent == 0)
+		{
+			// reset debug text
+			m_DebugText = "";
+		}
 
 		if(chunkIdx > 0)
 		{
