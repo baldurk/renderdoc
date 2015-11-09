ca9293c576dde3b939dbffaed2fbfa6ac3f6c2d1
baldurk <baldurk@baldurk.org>
2015-11-09 23:05:02 +0100

    Fix File->Exit shortcut clashing with File->Recent Capture Settings

--- renderdocui/Windows/MainWindow.Designer.cs
+++ renderdocui/Windows/MainWindow.Designer.cs
@@ -266,7 +266,7 @@
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
-            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Text = "E&xit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // windowToolStripMenuItem
