90e96e2fd686233130500a07102574c3df6238e5
baldurk <baldurk@baldurk.org>
2016-04-24 13:37:36 +0200

    Slight increase in tolerance on overdraw ramp decoding

--- renderdoc/replay/replay_output.cpp
+++ renderdoc/replay/replay_output.cpp
@@ -261,9 +261,9 @@ bool ReplayOutput::PickPixel(ResourceId tex, bool customShader, uint32_t x, uint
 	{
 		for(size_t c=0; c < ARRAY_COUNT(overdrawRamp); c++)
 		{
-			if(fabs(ret->value_f[0] - overdrawRamp[c].x) < 0.00004f &&
-				 fabs(ret->value_f[1] - overdrawRamp[c].y) < 0.00004f &&
-				 fabs(ret->value_f[2] - overdrawRamp[c].z) < 0.00004f)
+			if(fabs(ret->value_f[0] - overdrawRamp[c].x) < 0.00005f &&
+				 fabs(ret->value_f[1] - overdrawRamp[c].y) < 0.00005f &&
+				 fabs(ret->value_f[2] - overdrawRamp[c].z) < 0.00005f)
 			{
 				ret->value_i[0] = (int32_t)c;
 				ret->value_i[1] = 0;
