2f07027386b8962e92c3e1f953178f669d1ef51d
Bart Muzzin <bart.muzzin@autodesk.com>
2016-06-06 15:29:05 -0400

    Always write pPropertyCount value when returning properties. Refs #279

--- renderdoc/driver/vulkan/vk_core.cpp
+++ renderdoc/driver/vulkan/vk_core.cpp
@@ -644,6 +644,9 @@ static VkResult FillPropertyCountAndList(const VkExtensionProperties *src, uint3
   {
     uint32_t dstSpace = *dstCount;
 
+    // return the number of extensions.
+    *dstCount = numExts;
+
     // copy as much as there's space for, up to how many there are
     memcpy(dstProps, src, sizeof(VkExtensionProperties) * RDCMIN(numExts, dstSpace));
 
