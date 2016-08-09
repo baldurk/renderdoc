6b01ab8136c2960f877093e218095589d2d6238d
Hubert Jarosz <marqin.pl@gmail.com>
2016-08-09 23:04:19 +0200

    add CMake funcion for determining current git rev
    /GetGitHash.cmake has function get_git_hash that gives current
    git rev hash or "NO_GIT_COMMIT_HASH_DEFINED" if we are
    building out of git repo.
    It's used in /renderdoc/CMakeLists.txt

---
+++ GetGitHash.cmake
@@ -0,0 +1,13 @@
+function(get_git_hash _git_hash)
+  if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    execute_process(
+      COMMAND git rev-parse HEAD
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+      OUTPUT_VARIABLE GIT_HASH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  else()
+    set(GIT_HASH "NO_GIT_COMMIT_HASH_DEFINED")
+  endif()
+  set(${_git_hash} "${GIT_HASH}" PARENT_SCOPE)
+endfunction(get_git_hash)
