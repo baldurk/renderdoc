d39d2ed9dd82992f55ece209f491e21728cd3175
baldurk <baldurk@baldurk.org>
2016-09-27 12:55:08 +0200

    Roll calc_hash.sh into hash_version.sh (the only place that it's used)

--- hash_version.sh
+++ hash_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GIT_HASH=`sh ./calc_hash.sh`;
+GIT_HASH=`git status > /dev/null 2>&1 && git rev-parse HEAD || echo NO_GIT_COMMIT_HASH_DEFINED`;
 
 rm -f ver
 sed -b "s/NO_GIT_COMMIT_HASH_DEFINED/$GIT_HASH$1/" renderdoc/data/resource.h > ver && mv ver renderdoc/data/resource.h
