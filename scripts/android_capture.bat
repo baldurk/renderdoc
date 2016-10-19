8856d3a6f2ff465ae6d364480d0f50d047f85ca4
Michael Rennie <m.rennie@partner.samsung.com>
2016-10-19 16:07:58 +0100

    Force-stop Android package before starting it for capture.

--- scripts/android_capture.bat
+++ scripts/android_capture.bat
@@ -1,3 +1,4 @@
+adb shell am force-stop %*
 adb forward tcp:38970 tcp:38920
 adb shell setprop debug.vulkan.layers VK_LAYER_RENDERDOC_Capture
 adb shell monkey -p %* -c android.intent.category.LAUNCHER 1
