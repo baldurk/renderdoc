f9e3e970187d679f19b01bd15fb9de6f24c6942e
Peter Gal <pgal.u-szeged@partner.samsung.com>
2016-11-10 19:18:01 +0100

    Re-enable the D3D11 replay backend

--- renderdoc/driver/d3d11/d3d11_replay.cpp
+++ renderdoc/driver/d3d11/d3d11_replay.cpp
@@ -2115,7 +2115,4 @@ ReplayCreateStatus D3D11_CreateReplayDevice(const char *logfile, IReplayDriver *
   return eReplayCreate_APIHardwareUnsupported;
 }
 
-/* // TODO(elecro): Disable the D3D11 device for now
-
 static DriverRegistration D3D11DriverRegistration(RDC_D3D11, "D3D11", &D3D11_CreateReplayDevice);
-*/
