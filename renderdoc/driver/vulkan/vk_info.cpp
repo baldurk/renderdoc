6c348c65c2b3b0c79a86e733c646f278bb783deb
baldurk <baldurk@baldurk.org>
2017-06-17 01:10:35 +0100

    Fix clang formatting error that somehow snuck in

--- renderdoc/driver/vulkan/vk_info.cpp
+++ renderdoc/driver/vulkan/vk_info.cpp
@@ -467,7 +467,8 @@ void VulkanCreationInfo::RenderPass::Init(VulkanResourceManager *resourceMan,
     dst.colorLayouts.resize(src.colorAttachmentCount);
     for(uint32_t i = 0; i < src.colorAttachmentCount; i++)
     {
-      dst.resolveAttachments[i] = src.pResolveAttachments ? src.pResolveAttachments[i].attachment : ~0U;
+      dst.resolveAttachments[i] =
+          src.pResolveAttachments ? src.pResolveAttachments[i].attachment : ~0U;
       dst.colorAttachments[i] = src.pColorAttachments[i].attachment;
       dst.colorLayouts[i] = src.pColorAttachments[i].layout;
     }
