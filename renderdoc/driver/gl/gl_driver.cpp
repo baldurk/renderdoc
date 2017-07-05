baldurk/renderdoc

commit 494427aac4b94b2283bea6bedba07d1aad6dbac7

Save client-memory vertex and indices when using indexed draws.
Common_pre/postElements merged into CopyClientMemoryArrays.

renderdoc/driver/gl/gl_driver.cpp
@@ -909,7 +909,6 @@ WrappedOpenGL::WrappedOpenGL(const char *logfile, const GLHookSet &funcs, GLPlat
   m_FakeBB_Color = 0;
   m_FakeBB_DepthStencil = 0;
   m_FakeVAO = 0;
-  m_FakeIdxBuf = 0;
   m_FakeIdxSize = 0;
 
   m_CurChunkOffset = 0;
@@ -939,14 +938,6 @@ void WrappedOpenGL::Initialise(GLInitParams &params)
   gl.glGenVertexArrays(1, &m_FakeVAO);
   gl.glBindVertexArray(m_FakeVAO);
   gl.glBindVertexArray(0);
-
-  // we use this to draw from index data that was 'immediate' passed to the
-  // draw function, as i na real memory pointer
-  gl.glGenBuffers(1, &m_FakeIdxBuf);
-  gl.glBindBuffer(eGL_ELEMENT_ARRAY_BUFFER, m_FakeIdxBuf);
-  m_FakeIdxSize = 1024 * 1024;    // this buffer is resized up as needed
-  gl.glNamedBufferDataEXT(m_FakeIdxBuf, m_FakeIdxSize, NULL, eGL_DYNAMIC_DRAW);
-  gl.glBindBuffer(eGL_ELEMENT_ARRAY_BUFFER, 0);
 
   gl.glGenFramebuffers(1, &m_FakeBB_FBO);
   gl.glBindFramebuffer(eGL_FRAMEBUFFER, m_FakeBB_FBO);
@@ -1087,8 +1078,6 @@ string ToStrHelper<false, GLChunkType>::Get(const GLChunkType &el)
 
 WrappedOpenGL::~WrappedOpenGL()
 {
-  if(m_FakeIdxBuf)
-    m_Real.glDeleteBuffers(1, &m_FakeIdxBuf);
   if(m_FakeVAO)
     m_Real.glDeleteVertexArrays(1, &m_FakeVAO);
   if(m_FakeBB_FBO)
@@ -1162,6 +1151,8 @@ void WrappedOpenGL::DeleteContext(void *contextHandle)
 
   if(ctxdata.m_ClientMemoryVBOs[0])
     glDeleteBuffers(ARRAY_COUNT(ctxdata.m_ClientMemoryVBOs), ctxdata.m_ClientMemoryVBOs);
+  if(ctxdata.m_ClientMemoryIBO)
+    glDeleteBuffers(1, &ctxdata.m_ClientMemoryIBO);
 
   for(auto it = m_LastContexts.begin(); it != m_LastContexts.end(); ++it)
   {
@@ -1558,6 +1549,7 @@ void WrappedOpenGL::ActivateContext(GLWindowingData winData)
           glBufferData(eGL_ARRAY_BUFFER, 64, NULL, eGL_DYNAMIC_DRAW);
         }
         glBindBuffer(eGL_ARRAY_BUFFER, prevArrayBuffer);
+        glGenBuffers(1, &ctxdata.m_ClientMemoryIBO);
       }
     }
   }

renderdoc/driver/gl/gl_driver.h
@@ -347,7 +347,6 @@ private:
   GLuint m_FakeBB_Color;
   GLuint m_FakeBB_DepthStencil;
   GLuint m_FakeVAO;
-  GLuint m_FakeIdxBuf;
   GLsizeiptr m_FakeIdxSize;
 
   ResourceId m_FakeVAOID;
@@ -395,6 +394,7 @@ private:
       m_TextureUnit = 0;
       m_ProgramPipeline = m_Program = 0;
       RDCEraseEl(m_ClientMemoryVBOs);
+      m_ClientMemoryIBO = 0;
     }
 
     void *ctx;
@@ -453,6 +453,7 @@ private:
     // GLES allows drawing from client memory, in which case we will copy to
     // temporary VBOs so that input mesh data is recorded. See struct ClientMemoryData
     GLuint m_ClientMemoryVBOs[16];
+    GLuint m_ClientMemoryIBO;
   };
 
   struct ClientMemoryData
@@ -469,8 +470,9 @@ private:
     std::vector<VertexAttrib> attribs;
     GLuint prevArrayBufferBinding;
   };
-  ClientMemoryData *CopyClientMemoryArrays(GLint first, GLsizei count);
-  void RestoreClientMemoryArrays(ClientMemoryData *clientMemoryArrays);
+  ClientMemoryData *CopyClientMemoryArrays(GLint first, GLsizei count, GLenum indexType,
+                                           const void *&indices);
+  void RestoreClientMemoryArrays(ClientMemoryData *clientMemoryArrays, GLenum indexType);
 
   map<void *, ContextData> m_ContextData;
 
@@ -1740,12 +1742,6 @@ public:
   IMPLEMENT_FUNCTION_SERIALISED(void, glProgramUniformMatrix4x3dv(GLuint program, GLint location,
                                                                   GLsizei count, GLboolean transpose,
                                                                   const GLdouble *value));
-
-  // utility handling functions for glDraw*Elements* to handle pointers to indices being
-  // passed directly, with no index buffer bound. It's not allowed in core profile but
-  // it's fairly common and not too hard to support
-  byte *Common_preElements(GLsizei Count, GLenum Type, uint64_t &IdxOffset);
-  void Common_postElements(byte *idxDelete);
 
   // final check function to ensure we don't try and render with no index buffer bound
   bool Check_preElements();

commit e2a624e35927eeb694f0ca62877a86c9e83abe15

Add backwards compatibility code for breakage in 494427aac4b94b
* When Common_preElements was removed, it lost some serialised data.
  Although we don't need it anymore, we at least need to serialise the
  bool indicating no data came from memory so that the serialisation
  lines up.

renderdoc/driver/gl/gl_driver.cpp
@@ -351,6 +351,8 @@ const uint32_t GLInitParams::GL_OLD_VERSIONS[GLInitParams::GL_NUM_SUPPORTED_OLD_
     0x000013,    // Serialised vertex attribute and fragdata bindings for programs as initial
                  // contents data
     0x000014,    // Added support for primitive bounding boxes on GLES
+    0x000015,    // Changed serialisation of client-side index buffers which removed a bool even
+                 // when they aren't used.
 };
 
 ReplayStatus GLInitParams::Serialise()

renderdoc/driver/gl/gl_driver.h
@@ -53,10 +53,10 @@ struct GLInitParams : public RDCInitParams
   uint32_t width;
   uint32_t height;
 
-  static const uint32_t GL_SERIALISE_VERSION = 0x0000015;
+  static const uint32_t GL_SERIALISE_VERSION = 0x0000016;
 
   // backwards compatibility for old logs described at the declaration of this array
-  static const uint32_t GL_NUM_SUPPORTED_OLD_VERSIONS = 5;
+  static const uint32_t GL_NUM_SUPPORTED_OLD_VERSIONS = 6;
   static const uint32_t GL_OLD_VERSIONS[GL_NUM_SUPPORTED_OLD_VERSIONS];
 
   // version number internal to opengl stream
@@ -474,6 +474,9 @@ private:
   ClientMemoryData *CopyClientMemoryArrays(GLint first, GLsizei count, GLenum indexType,
                                            const void *&indices);
   void RestoreClientMemoryArrays(ClientMemoryData *clientMemoryArrays, GLenum indexType);
+
+  // used to match serialisation with older GL captures
+  void Legacy_preElements(GLenum Type, uint32_t Count);
 
   map<void *, ContextData> m_ContextData;
 

renderdoc/driver/gl/wrappers/gl_draw_funcs.cpp
@@ -1121,6 +1121,36 @@ bool WrappedOpenGL::Check_preElements()
   return true;
 }
 
+void WrappedOpenGL::Legacy_preElements(GLenum Type, uint32_t Count)
+{
+  if(m_State <= EXECUTING && GetLogVersion() <= 0x000015)
+  {
+    // in older logs there used to be a different way of manually saving client-side memory
+    // indices.
+    // We don't support replaying this anymore, but we need to match serialisation to be able to
+    // open older captures - in 99% of cases the bool will be false. When it's true, we just add
+    // an error message about it.
+    SERIALISE_ELEMENT(bool, IndicesFromMemory, false);
+
+    if(IndicesFromMemory)
+    {
+      uint32_t IdxSize = Type == eGL_UNSIGNED_BYTE ? 1 : Type == eGL_UNSIGNED_SHORT
+                                                             ? 2
+                                                             : /*Type == eGL_UNSIGNED_INT*/ 4;
+
+      // serialise the data, even unused
+      SERIALISE_ELEMENT_BUF(byte *, idxdata, NULL, size_t(IdxSize * Count));
+
+      AddDebugMessage(MessageCategory::Deprecated, MessageSeverity::High,
+                      MessageSource::UnsupportedConfiguration,
+                      "Client-side index data used at drawcall, re-capture with a new version to "
+                      "replay this draw.");
+
+      SAFE_DELETE_ARRAY(idxdata);
+    }
+  }
+}
+
 bool WrappedOpenGL::Serialise_glDrawElements(GLenum mode, GLsizei count, GLenum type,
                                              const void *indices)
 {
@@ -1131,6 +1161,8 @@ bool WrappedOpenGL::Serialise_glDrawElements(GLenum mode, GLsizei count, GLenum 
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawElements(Mode, Count, Type, (const void *)IdxOffset);
   }
@@ -1285,6 +1317,8 @@ bool WrappedOpenGL::Serialise_glDrawRangeElements(GLenum mode, GLuint start, GLu
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawRangeElements(Mode, Start, End, Count, Type, (const void *)IdxOffset);
   }
@@ -1364,6 +1398,8 @@ bool WrappedOpenGL::Serialise_glDrawRangeElementsBaseVertex(GLenum mode, GLuint 
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawRangeElementsBaseVertex(Mode, Start, End, Count, Type, (const void *)IdxOffset,
                                            BaseVtx);
@@ -1442,6 +1478,8 @@ bool WrappedOpenGL::Serialise_glDrawElementsBaseVertex(GLenum mode, GLsizei coun
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawElementsBaseVertex(Mode, Count, Type, (const void *)IdxOffset, BaseVtx);
   }
@@ -1518,6 +1556,8 @@ bool WrappedOpenGL::Serialise_glDrawElementsInstanced(GLenum mode, GLsizei count
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawElementsInstanced(Mode, Count, Type, (const void *)IdxOffset, InstCount);
   }
@@ -1597,6 +1637,8 @@ bool WrappedOpenGL::Serialise_glDrawElementsInstancedBaseInstance(GLenum mode, G
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawElementsInstancedBaseInstance(Mode, Count, Type, (const void *)IdxOffset,
                                                  InstCount, BaseInstance);
@@ -1679,6 +1721,8 @@ bool WrappedOpenGL::Serialise_glDrawElementsInstancedBaseVertex(GLenum mode, GLs
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawElementsInstancedBaseVertex(Mode, Count, Type, (const void *)IdxOffset,
                                                InstCount, BaseVertex);
@@ -1762,6 +1806,8 @@ bool WrappedOpenGL::Serialise_glDrawElementsInstancedBaseVertexBaseInstance(
 
   if(m_State <= EXECUTING)
   {
+    Legacy_preElements(Type, Count);
+
     if(Check_preElements())
       m_Real.glDrawElementsInstancedBaseVertexBaseInstance(
           Mode, Count, Type, (const void *)IdxOffset, InstCount, BaseVertex, BaseInstance);