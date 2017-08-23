8478bfbb525f706233e98865bf1da0a862793e5e
Matthäus G. Chajdas <Matthaeus.Chajdas@amd.com>
2017-08-23 15:14:47 +0200

    Address review feedback.

--- qrenderdoc/Windows/Dialogs/PerformanceCounterSelection.h
+++ qrenderdoc/Windows/Dialogs/PerformanceCounterSelection.h
@@ -44,11 +44,20 @@ public:
 
   QList<GPUCounter> GetSelectedCounters() const;
 
+public slots:
+  void Save();
+  void Load();
+
 private:
+  void SetCounters(const QVector<CounterDescription> &descriptions);
+
   Ui::PerformanceCounterSelection *ui;
 
   ICaptureContext &m_Ctx;
   QMap<GPUCounter, QListWidgetItem *> m_SelectedCounters;
   QMap<GPUCounter, Uuid> m_CounterToUuid;
   QMap<Uuid, GPUCounter> m_UuidToCounter;
+
+  static const int CounterDescriptionRole;
+  static const int CounterIdRole;
 };
