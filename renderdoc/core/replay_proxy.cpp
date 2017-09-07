baldurk/renderdoc

commit 9b4ee72af6249f26d1188c1ed91fc71ddf163e23

Pass pipeline state around by reference instead of copying by value

renderdoc/core/replay_proxy.cpp
@@ -2443,6 +2443,51 @@ void ReplayProxy::SavePipelineState()
   m_FromReplaySerialiser->Serialise("", m_D3D12PipelineState);
   m_FromReplaySerialiser->Serialise("", m_GLPipelineState);
   m_FromReplaySerialiser->Serialise("", m_VulkanPipelineState);
+
+  {
+    D3D11Pipe::Shader *stages[] = {
+        &m_D3D11PipelineState.m_VS, &m_D3D11PipelineState.m_HS, &m_D3D11PipelineState.m_DS,
+        &m_D3D11PipelineState.m_GS, &m_D3D11PipelineState.m_PS, &m_D3D11PipelineState.m_CS,
+    };
+
+    for(int i = 0; i < 6; i++)
+      if(stages[i]->Object != ResourceId())
+        stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
+  }
+
+  {
+    D3D12Pipe::Shader *stages[] = {
+        &m_D3D12PipelineState.m_VS, &m_D3D12PipelineState.m_HS, &m_D3D12PipelineState.m_DS,
+        &m_D3D12PipelineState.m_GS, &m_D3D12PipelineState.m_PS, &m_D3D12PipelineState.m_CS,
+    };
+
+    for(int i = 0; i < 6; i++)
+      if(stages[i]->Object != ResourceId())
+        stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
+  }
+
+  {
+    GLPipe::Shader *stages[] = {
+        &m_GLPipelineState.m_VS, &m_GLPipelineState.m_TCS, &m_GLPipelineState.m_TES,
+        &m_GLPipelineState.m_GS, &m_GLPipelineState.m_FS,  &m_GLPipelineState.m_CS,
+    };
+
+    for(int i = 0; i < 6; i++)
+      if(stages[i]->Object != ResourceId())
+        stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
+  }
+
+  {
+    VKPipe::Shader *stages[] = {
+        &m_VulkanPipelineState.m_VS, &m_VulkanPipelineState.m_TCS, &m_VulkanPipelineState.m_TES,
+        &m_VulkanPipelineState.m_GS, &m_VulkanPipelineState.m_FS,  &m_VulkanPipelineState.m_CS,
+    };
+
+    for(int i = 0; i < 6; i++)
+      if(stages[i]->Object != ResourceId())
+        stages[i]->ShaderDetails =
+            GetShader(GetLiveID(stages[i]->Object), stages[i]->entryPoint.elems);
+  }
 }
 
 void ReplayProxy::ReplayLog(uint32_t endEventID, ReplayLogType replayType)

qrenderdoc/Code/CaptureContext.cpp
@@ -263,12 +263,12 @@ void CaptureContext::LoadLogfileThreaded(const QString &logFile, const QString &
 
     m_PostloadProgress = 0.9f;
 
-    m_CurD3D11PipelineState = r->GetD3D11PipelineState();
-    m_CurD3D12PipelineState = r->GetD3D12PipelineState();
-    m_CurGLPipelineState = r->GetGLPipelineState();
-    m_CurVulkanPipelineState = r->GetVulkanPipelineState();
-    m_CurPipelineState.SetStates(m_APIProps, &m_CurD3D11PipelineState, &m_CurD3D12PipelineState,
-                                 &m_CurGLPipelineState, &m_CurVulkanPipelineState);
+    m_CurD3D11PipelineState = &r->GetD3D11PipelineState();
+    m_CurD3D12PipelineState = &r->GetD3D12PipelineState();
+    m_CurGLPipelineState = &r->GetGLPipelineState();
+    m_CurVulkanPipelineState = &r->GetVulkanPipelineState();
+    m_CurPipelineState.SetStates(m_APIProps, m_CurD3D11PipelineState, m_CurD3D12PipelineState,
+                                 m_CurGLPipelineState, m_CurVulkanPipelineState);
 
     m_UnreadMessageCount = 0;
     AddMessages(m_FrameInfo.debugMessages);
@@ -534,11 +534,12 @@ void CaptureContext::CloseLogfile()
   m_Drawcalls.clear();
   m_FirstDrawcall = m_LastDrawcall = NULL;
 
-  m_CurD3D11PipelineState = D3D11Pipe::State();
-  m_CurD3D12PipelineState = D3D12Pipe::State();
-  m_CurGLPipelineState = GLPipe::State();
-  m_CurVulkanPipelineState = VKPipe::State();
-  m_CurPipelineState.SetStates(m_APIProps, NULL, NULL, NULL, NULL);
+  m_CurD3D11PipelineState = NULL;
+  m_CurD3D12PipelineState = NULL;
+  m_CurGLPipelineState = NULL;
+  m_CurVulkanPipelineState = NULL;
+  m_CurPipelineState.SetStates(m_APIProps, m_CurD3D11PipelineState, m_CurD3D12PipelineState,
+                               m_CurGLPipelineState, m_CurVulkanPipelineState);
 
   m_DebugMessages.clear();
   m_UnreadMessageCount = 0;
@@ -564,12 +565,12 @@ void CaptureContext::SetEventID(const QVector<ILogViewer *> &exclude, uint32_t s
 
   m_Renderer.BlockInvoke([this, eventID, force](IReplayController *r) {
     r->SetFrameEvent(eventID, force);
-    m_CurD3D11PipelineState = r->GetD3D11PipelineState();
-    m_CurD3D12PipelineState = r->GetD3D12PipelineState();
-    m_CurGLPipelineState = r->GetGLPipelineState();
-    m_CurVulkanPipelineState = r->GetVulkanPipelineState();
-    m_CurPipelineState.SetStates(m_APIProps, &m_CurD3D11PipelineState, &m_CurD3D12PipelineState,
-                                 &m_CurGLPipelineState, &m_CurVulkanPipelineState);
+    m_CurD3D11PipelineState = &r->GetD3D11PipelineState();
+    m_CurD3D12PipelineState = &r->GetD3D12PipelineState();
+    m_CurGLPipelineState = &r->GetGLPipelineState();
+    m_CurVulkanPipelineState = &r->GetVulkanPipelineState();
+    m_CurPipelineState.SetStates(m_APIProps, m_CurD3D11PipelineState, m_CurD3D12PipelineState,
+                                 m_CurGLPipelineState, m_CurVulkanPipelineState);
   });
 
   for(ILogViewer *logviewer : m_LogViewers)

qrenderdoc/Code/CaptureContext.h
@@ -186,19 +186,19 @@ public:
   void AddDockWindow(QWidget *newWindow, DockReference ref, QWidget *refWindow,
                      float percentage = 0.5f) override;
 
-  D3D11Pipe::State &CurD3D11PipelineState() override { return m_CurD3D11PipelineState; }
-  D3D12Pipe::State &CurD3D12PipelineState() override { return m_CurD3D12PipelineState; }
-  GLPipe::State &CurGLPipelineState() override { return m_CurGLPipelineState; }
-  VKPipe::State &CurVulkanPipelineState() override { return m_CurVulkanPipelineState; }
+  const D3D11Pipe::State &CurD3D11PipelineState() override { return *m_CurD3D11PipelineState; }
+  const D3D12Pipe::State &CurD3D12PipelineState() override { return *m_CurD3D12PipelineState; }
+  const GLPipe::State &CurGLPipelineState() override { return *m_CurGLPipelineState; }
+  const VKPipe::State &CurVulkanPipelineState() override { return *m_CurVulkanPipelineState; }
   CommonPipelineState &CurPipelineState() override { return m_CurPipelineState; }
   PersistantConfig &Config() override { return m_Config; }
 private:
   ReplayManager m_Renderer;
 
-  D3D11Pipe::State m_CurD3D11PipelineState;
-  D3D12Pipe::State m_CurD3D12PipelineState;
-  GLPipe::State m_CurGLPipelineState;
-  VKPipe::State m_CurVulkanPipelineState;
+  const D3D11Pipe::State *m_CurD3D11PipelineState;
+  const D3D12Pipe::State *m_CurD3D12PipelineState;
+  const GLPipe::State *m_CurGLPipelineState;
+  const VKPipe::State *m_CurVulkanPipelineState;
   CommonPipelineState m_CurPipelineState;
 
   PersistantConfig &m_Config;

qrenderdoc/Code/Interface/CommonPipelineState.cpp
@@ -928,7 +928,7 @@ BoundCBuffer CommonPipelineState::GetConstantBuffer(ShaderStage stage, uint32_t 
               s.BindpointMapping.ConstantBlocks[s.ShaderDetails->ConstantBlocks[BufIdx].bindPoint].bind;
           if(uboIdx >= 0 && uboIdx < m_GL->UniformBuffers.count)
           {
-            GLPipe::Buffer &b = m_GL->UniformBuffers[uboIdx];
+            const GLPipe::Buffer &b = m_GL->UniformBuffers[uboIdx];
 
             buf = b.Resource;
             ByteOffset = b.Offset;
@@ -939,7 +939,8 @@ BoundCBuffer CommonPipelineState::GetConstantBuffer(ShaderStage stage, uint32_t 
     }
     else if(IsLogVK())
     {
-      VKPipe::Pipeline &pipe = stage == ShaderStage::Compute ? m_Vulkan->compute : m_Vulkan->graphics;
+      const VKPipe::Pipeline &pipe =
+          stage == ShaderStage::Compute ? m_Vulkan->compute : m_Vulkan->graphics;
       const VKPipe::Shader &s = GetVulkanStage(stage);
 
       if(s.ShaderDetails != NULL && BufIdx < (uint32_t)s.ShaderDetails->ConstantBlocks.count)

qrenderdoc/Code/Interface/CommonPipelineState.h
@@ -145,8 +145,8 @@ public:
 :param ~renderdoc.GL_State gl: The OpenGL state.
 :param ~renderdoc.VK_State vk: The Vulkan state.
 )");
-  void SetStates(APIProperties props, D3D11Pipe::State *d3d11, D3D12Pipe::State *d3d12,
-                 GLPipe::State *gl, VKPipe::State *vk)
+  void SetStates(APIProperties props, const D3D11Pipe::State *d3d11, const D3D12Pipe::State *d3d12,
+                 const GLPipe::State *gl, const VKPipe::State *vk)
   {
     m_APIProps = props;
     m_D3D11 = d3d11;
@@ -422,10 +422,10 @@ Typically this is ``glsl`` or ``hlsl``.
   QVector<BoundResource> GetOutputTargets();
 
 private:
-  D3D11Pipe::State *m_D3D11 = NULL;
-  D3D12Pipe::State *m_D3D12 = NULL;
-  GLPipe::State *m_GL = NULL;
-  VKPipe::State *m_Vulkan = NULL;
+  const D3D11Pipe::State *m_D3D11 = NULL;
+  const D3D12Pipe::State *m_D3D12 = NULL;
+  const GLPipe::State *m_GL = NULL;
+  const VKPipe::State *m_Vulkan = NULL;
   APIProperties m_APIProps;
 
   const D3D11Pipe::Shader &GetD3D11Stage(ShaderStage stage);

qrenderdoc/Code/Interface/QRDInterface.h
@@ -1386,28 +1386,28 @@ currently docked.
 :return: The current D3D11 pipeline state.
 :rtype: ~renderdoc.D3D11_State
 )");
-  virtual D3D11Pipe::State &CurD3D11PipelineState() = 0;
+  virtual const D3D11Pipe::State &CurD3D11PipelineState() = 0;
 
   DOCUMENT(R"(Retrieve the current :class:`~renderdoc.D3D12_State` pipeline state.
 
 :return: The current D3D12 pipeline state.
 :rtype: ~renderdoc.D3D12_State
 )");
-  virtual D3D12Pipe::State &CurD3D12PipelineState() = 0;
+  virtual const D3D12Pipe::State &CurD3D12PipelineState() = 0;
 
   DOCUMENT(R"(Retrieve the current :class:`~renderdoc.GL_State` pipeline state.
 
 :return: The current OpenGL pipeline state.
 :rtype: ~renderdoc.GL_State
 )");
-  virtual GLPipe::State &CurGLPipelineState() = 0;
+  virtual const GLPipe::State &CurGLPipelineState() = 0;
 
   DOCUMENT(R"(Retrieve the current :class:`~renderdoc.VK_State` pipeline state.
 
 :return: The current Vulkan pipeline state.
 :rtype: ~renderdoc.VK_State
 )");
-  virtual VKPipe::State &CurVulkanPipelineState() = 0;
+  virtual const VKPipe::State &CurVulkanPipelineState() = 0;
 
   DOCUMENT(R"(Retrieve the current :class:`CommonPipelineState` abstracted pipeline state.
 

qrenderdoc/Windows/PipelineState/D3D11PipelineStateViewer.cpp
@@ -2261,7 +2261,7 @@ void D3D11PipelineStateViewer::shaderSave_clicked()
   m_Common.SaveShaderFile(shaderDetails);
 }
 
-QVariantList D3D11PipelineStateViewer::exportViewHTML(D3D11Pipe::View &view, int i,
+QVariantList D3D11PipelineStateViewer::exportViewHTML(const D3D11Pipe::View &view, int i,
                                                       ShaderReflection *refl,
                                                       const QString &extraParams)
 {
@@ -2385,7 +2385,7 @@ QVariantList D3D11PipelineStateViewer::exportViewHTML(D3D11Pipe::View &view, int
           d, a,    viewFormat,        format,   viewParams};
 }
 
-void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::IA &ia)
+void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::IA &ia)
 {
   {
     xml.writeStartElement(lit("h3"));
@@ -2483,7 +2483,7 @@ void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::IA &
   m_Common.exportHTMLTable(xml, {tr("Primitive Topology")}, {ToQStr(m_Ctx.CurDrawcall()->topology)});
 }
 
-void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::Shader &sh)
+void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::Shader &sh)
 {
   ShaderReflection *shaderDetails = sh.ShaderDetails;
 
@@ -2697,7 +2697,7 @@ void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::Shad
   }
 }
 
-void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::SO &so)
+void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::SO &so)
 {
   {
     xml.writeStartElement(lit("h3"));
@@ -2735,7 +2735,7 @@ void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::SO &
   }
 }
 
-void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::Rasterizer &rs)
+void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::Rasterizer &rs)
 {
   {
     xml.writeStartElement(lit("h3"));
@@ -2812,7 +2812,7 @@ void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::Rast
   }
 }
 
-void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, D3D11Pipe::OM &om)
+void D3D11PipelineStateViewer::exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::OM &om)
 {
   {
     xml.writeStartElement(lit("h3"));

qrenderdoc/Windows/PipelineState/D3D11PipelineStateViewer.h
@@ -93,13 +93,13 @@ private:
   void setState();
   void clearState();
 
-  QVariantList exportViewHTML(D3D11Pipe::View &view, int i, ShaderReflection *refl,
+  QVariantList exportViewHTML(const D3D11Pipe::View &view, int i, ShaderReflection *refl,
                               const QString &extraParams);
-  void exportHTML(QXmlStreamWriter &xml, D3D11Pipe::IA &ia);
-  void exportHTML(QXmlStreamWriter &xml, D3D11Pipe::Shader &sh);
-  void exportHTML(QXmlStreamWriter &xml, D3D11Pipe::SO &so);
-  void exportHTML(QXmlStreamWriter &xml, D3D11Pipe::Rasterizer &rs);
-  void exportHTML(QXmlStreamWriter &xml, D3D11Pipe::OM &om);
+  void exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::IA &ia);
+  void exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::Shader &sh);
+  void exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::SO &so);
+  void exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::Rasterizer &rs);
+  void exportHTML(QXmlStreamWriter &xml, const D3D11Pipe::OM &om);
 
   void setInactiveRow(RDTreeWidgetItem *node);
   void setEmptyRow(RDTreeWidgetItem *node);

commit e8ce6f3319b42cb06d552004c4d63c1acd0aaa14

Fix remote replay issue caused by 9b4ee72. Refs #735

renderdoc/core/replay_proxy.cpp
@@ -2489,49 +2489,52 @@ void ReplayProxy::SavePipelineState()
   m_FromReplaySerialiser->Serialise("", m_GLPipelineState);
   m_FromReplaySerialiser->Serialise("", m_VulkanPipelineState);
 
+  if(!m_RemoteServer)
   {
-    D3D11Pipe::Shader *stages[] = {
-        &m_D3D11PipelineState.m_VS, &m_D3D11PipelineState.m_HS, &m_D3D11PipelineState.m_DS,
-        &m_D3D11PipelineState.m_GS, &m_D3D11PipelineState.m_PS, &m_D3D11PipelineState.m_CS,
-    };
+    {
+      D3D11Pipe::Shader *stages[] = {
+          &m_D3D11PipelineState.m_VS, &m_D3D11PipelineState.m_HS, &m_D3D11PipelineState.m_DS,
+          &m_D3D11PipelineState.m_GS, &m_D3D11PipelineState.m_PS, &m_D3D11PipelineState.m_CS,
+      };
 
-    for(int i = 0; i < 6; i++)
-      if(stages[i]->Object != ResourceId())
-        stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
-  }
+      for(int i = 0; i < 6; i++)
+        if(stages[i]->Object != ResourceId())
+          stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
+    }
 
-  {
-    D3D12Pipe::Shader *stages[] = {
-        &m_D3D12PipelineState.m_VS, &m_D3D12PipelineState.m_HS, &m_D3D12PipelineState.m_DS,
-        &m_D3D12PipelineState.m_GS, &m_D3D12PipelineState.m_PS, &m_D3D12PipelineState.m_CS,
-    };
+    {
+      D3D12Pipe::Shader *stages[] = {
+          &m_D3D12PipelineState.m_VS, &m_D3D12PipelineState.m_HS, &m_D3D12PipelineState.m_DS,
+          &m_D3D12PipelineState.m_GS, &m_D3D12PipelineState.m_PS, &m_D3D12PipelineState.m_CS,
+      };
 
-    for(int i = 0; i < 6; i++)
-      if(stages[i]->Object != ResourceId())
-        stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
-  }
+      for(int i = 0; i < 6; i++)
+        if(stages[i]->Object != ResourceId())
+          stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
+    }
 
-  {
-    GLPipe::Shader *stages[] = {
-        &m_GLPipelineState.m_VS, &m_GLPipelineState.m_TCS, &m_GLPipelineState.m_TES,
-        &m_GLPipelineState.m_GS, &m_GLPipelineState.m_FS,  &m_GLPipelineState.m_CS,
-    };
+    {
+      GLPipe::Shader *stages[] = {
+          &m_GLPipelineState.m_VS, &m_GLPipelineState.m_TCS, &m_GLPipelineState.m_TES,
+          &m_GLPipelineState.m_GS, &m_GLPipelineState.m_FS,  &m_GLPipelineState.m_CS,
+      };
 
-    for(int i = 0; i < 6; i++)
-      if(stages[i]->Object != ResourceId())
-        stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
-  }
+      for(int i = 0; i < 6; i++)
+        if(stages[i]->Object != ResourceId())
+          stages[i]->ShaderDetails = GetShader(GetLiveID(stages[i]->Object), "");
+    }
 
-  {
-    VKPipe::Shader *stages[] = {
-        &m_VulkanPipelineState.m_VS, &m_VulkanPipelineState.m_TCS, &m_VulkanPipelineState.m_TES,
-        &m_VulkanPipelineState.m_GS, &m_VulkanPipelineState.m_FS,  &m_VulkanPipelineState.m_CS,
-    };
+    {
+      VKPipe::Shader *stages[] = {
+          &m_VulkanPipelineState.m_VS, &m_VulkanPipelineState.m_TCS, &m_VulkanPipelineState.m_TES,
+          &m_VulkanPipelineState.m_GS, &m_VulkanPipelineState.m_FS,  &m_VulkanPipelineState.m_CS,
+      };
 
-    for(int i = 0; i < 6; i++)
-      if(stages[i]->Object != ResourceId())
-        stages[i]->ShaderDetails =
-            GetShader(GetLiveID(stages[i]->Object), stages[i]->entryPoint.elems);
+      for(int i = 0; i < 6; i++)
+        if(stages[i]->Object != ResourceId())
+          stages[i]->ShaderDetails =
+              GetShader(GetLiveID(stages[i]->Object), stages[i]->entryPoint.elems);
+    }
   }
 }
 