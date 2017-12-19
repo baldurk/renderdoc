1c49f335a2b9438cbf74e263e64e283adb2bab70
baldurk <baldurk@baldurk.org>
2017-12-19 19:01:10 +0000

    In travis docs builds, do a minimal build of the python modules
    * We need to apt-get some build dependencies, and switch to python3 for
      the sphinx build so it can load the module.

--- scripts/travis/docs_compile.sh
+++ scripts/travis/docs_compile.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
+
+# ignore "error" codes in the env script below
+set +e
+
+. /opt/qt56/bin/qt56-env.sh
+
 set -ev
+
+mkdir build
+pushd build
+
+# Do a minimal build with as little as possible to get the python modules
+CC=gcc-6 CXX=g++-6 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
+make -j2
+
+popd
 
 cd docs/
 make html SPHINXOPTS=-W
--- scripts/travis/docs_setup.sh
+++ scripts/travis/docs_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -ev
 
-sudo pip install --upgrade pip setuptools
-sudo pip install Sphinx sphinx-rtd-theme
+sudo add-apt-repository -y 'ppa:ubuntu-toolchain-r/test'
+sudo add-apt-repository -y 'ppa:beineri/opt-qt562-trusty'
+sudo add-apt-repository -y 'deb http://apt.llvm.org/precise/ llvm-toolchain-precise-3.8 main'
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get install --allow-unauthenticated -y -qq libx11-dev mesa-common-dev libgl1-mesa-dev qt56base libxcb-keysyms1-dev gdb g++-6 python3-pip
+
+sudo pip3 install --upgrade pip setuptools
+sudo pip3 install Sphinx sphinx-rtd-theme
