e305029dddaef6c697f0fee5fc21fb751977d0a7
baldurk <baldurk@baldurk.org>
2018-01-01 17:55:29 +0000

    Update copyright years to 2018

--- renderdoc/driver/gl/hookset.pl
+++ renderdoc/driver/gl/hookset.pl
@@ -191,7 +191,7 @@ print <<ENDOFHEADER;
 /******************************************************************************
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2017 Baldur Karlsson
+ * Copyright (c) 2015-2018 Baldur Karlsson
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
