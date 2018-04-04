0ecc8ed0e5c0fbb82f8e95b72e99804136e56754
baldurk <baldurk@baldurk.org>
2018-04-04 17:53:28 +0100

    Fix a reported NULL pointer dereference when exporting GL state to HTML

--- qrenderdoc/Windows/PipelineState/GLPipelineStateViewer.cpp
+++ qrenderdoc/Windows/PipelineState/GLPipelineStateViewer.cpp
@@ -2637,21 +2637,20 @@ void GLPipelineStateViewer::exportHTML(QXmlStreamWriter &xml, const GLPipe::Shad
         uint64_t byteSize = shaderCBuf.byteSize;
 
         QString slotname = tr("Uniforms");
-        QString name = m_Ctx.GetResourceName(b->resourceId);
+        QString name = tr("Empty");
         QString sizestr = tr("%1 Variables").arg(numvars);
         QString byterange;
 
         if(!filledSlot)
-        {
-          name = tr("Empty");
           length = 0;
-        }
 
         if(b)
         {
           slotname = QFormatStr("%1: %2").arg(bindPoint).arg(shaderCBuf.name);
           offset = b->byteOffset;
           length = b->byteSize;
+
+          name = m_Ctx.GetResourceName(b->resourceId);
 
           BufferDescription *buf = m_Ctx.GetBuffer(b->resourceId);
           if(buf && length == 0)
