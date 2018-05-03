b8ffc1c79ff985288663f01a0178ade93f9f8531
baldurk <baldurk@baldurk.org>
2018-05-03 09:49:12 +0100

    Make sure to quote the subject when emailing

--- util/buildscripts/scripts/errormail.sh
+++ util/buildscripts/scripts/errormail.sh
@@ -20,5 +20,5 @@ if [ -f "${EMAILHOST}" ]; then
 	ssh $(cat "${EMAILHOST}") "mail -s \"$SUBJECT\" $ERRORMAIL" < $EMAIL_BODY_FILE
 	exit;
 else
-	mail -s $SUBJECT $ERRORMAIL < $EMAIL_BODY_FILE
+	mail -s \"$SUBJECT\" $ERRORMAIL < $EMAIL_BODY_FILE
 fi
