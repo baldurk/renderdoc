44f9b08e71d6a058ed46f1dc2bfbeb2a97a16b25
baldurk <baldurk@baldurk.org>
2018-07-06 11:56:47 +0100

    Tweak some core versions for extensions
    * ARB_geometry_shader4 was brought into desktop core GL in 3.2, not 4.1.
    * Geometry shaders and viewport arrays were in GLES 3.2

--- renderdoc/driver/gl/gl_common.h
+++ renderdoc/driver/gl/gl_common.h
@@ -603,9 +603,9 @@ extern bool IsGLES;
   EXT_TO_CHECK(40, 32, ARB_tessellation_shader)                  \
   EXT_TO_CHECK(40, 32, ARB_texture_cube_map_array)               \
   EXT_TO_CHECK(40, 30, ARB_transform_feedback2)                  \
-  EXT_TO_CHECK(41, 99, ARB_geometry_shader4)                     \
+  EXT_TO_CHECK(32, 32, ARB_geometry_shader4)                     \
   EXT_TO_CHECK(41, 31, ARB_separate_shader_objects)              \
-  EXT_TO_CHECK(41, 99, ARB_viewport_array)                       \
+  EXT_TO_CHECK(41, 32, ARB_viewport_array)                       \
   EXT_TO_CHECK(42, 99, ARB_base_instance)                        \
   EXT_TO_CHECK(42, 31, ARB_shader_atomic_counters)               \
   EXT_TO_CHECK(42, 31, ARB_shader_image_load_store)              \
