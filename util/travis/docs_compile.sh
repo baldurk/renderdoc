c49888424f04dd2307d6ad71f9fece894ee6b50a
baldurk <baldurk@baldurk.org>
2018-07-06 23:55:12 +0100

    Disable GLES compilation for docs build

--- util/travis/docs_compile.sh
+++ util/travis/docs_compile.sh
@@ -11,7 +11,7 @@ mkdir build
 pushd build
 
 # Do a minimal build with as little as possible to get the python modules
-CC=gcc-6 CXX=g++-6 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
+CC=gcc-6 CXX=g++-6 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_GLES=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
 make -j2
 
 popd
