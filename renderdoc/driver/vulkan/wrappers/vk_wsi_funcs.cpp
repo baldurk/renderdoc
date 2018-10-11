bd394bdd74b6da923a3583138bca5a8e4b472780
baldurk <baldurk@baldurk.org>
2018-10-11 13:57:25 +0100

    Allow VK_KHR_incremental_present
    * Untested, but present regions shouldn't affect our operation.

--- renderdoc/driver/vulkan/wrappers/vk_wsi_funcs.cpp
+++ renderdoc/driver/vulkan/wrappers/vk_wsi_funcs.cpp
@@ -651,7 +651,8 @@ VkResult WrappedVulkan::vkQueuePresentKHR(VkQueue queue, const VkPresentInfoKHR 
   {
     // allowed (and ignored) pNext structs
     if(next->sType != VK_STRUCTURE_TYPE_DISPLAY_PRESENT_INFO_KHR &&
-       next->sType != VK_STRUCTURE_TYPE_DEVICE_GROUP_PRESENT_INFO_KHR)
+       next->sType != VK_STRUCTURE_TYPE_DEVICE_GROUP_PRESENT_INFO_KHR &&
+       next->sType != VK_STRUCTURE_TYPE_PRESENT_REGIONS_KHR)
     {
       RDCWARN("Unsupported pNext structure in pPresentInfo: %s", ToStr(next->sType).c_str());
     }
