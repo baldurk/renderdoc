fa6b5c5d09379ba52878b54d103efd1c05f10bb3
baldurk <baldurk@baldurk.org>
2018-10-23 12:57:11 +0100

    Magically fix travis OSX compile
    * Probably there was a trailing non-zero exit code that's not actually fatal,
      from homebrew commands. The echo hides that.

--- util/travis/osx_compile.sh
+++ util/travis/osx_compile.sh
@@ -3,5 +3,4 @@
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-echo "Building with $(nproc) jobs"
 make -j2
--- util/travis/osx_setup.sh
+++ util/travis/osx_setup.sh
@@ -4,3 +4,5 @@ brew update
 brew install qt5
 brew link qt5 --force
 brew upgrade python
+
+echo "Setup complete"
