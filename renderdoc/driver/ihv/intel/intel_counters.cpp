3233afd3907f1ea39c70766a4dcaa87d43257861
baldurk <baldurk@baldurk.org>
2018-12-03 12:13:19 +0000

    If Intel metrics device version check fails, don't call invalid function

--- renderdoc/driver/ihv/intel/intel_counters.cpp
+++ renderdoc/driver/ihv/intel/intel_counters.cpp
@@ -45,14 +45,17 @@ IntelCounters::~IntelCounters()
 {
   SAFE_RELEASE(m_deviceContext);
 
-  if(CloseMetricsDevice)
-  {
+  if(CloseMetricsDevice && m_metricsDevice)
     CloseMetricsDevice(m_metricsDevice);
-    m_metricsDevice = NULL;
-  }
+
+  m_metricsDevice = NULL;
 
   if(m_MDLibraryHandle)
     FreeLibrary(m_MDLibraryHandle);
+
+  m_MDLibraryHandle = (HMODULE)0;
+  OpenMetricsDevice = NULL;
+  CloseMetricsDevice = NULL;
 }
 
 void IntelCounters::Load()
@@ -95,8 +98,11 @@ bool IntelCounters::Init(void *pContext)
   {
     CloseMetricsDevice(m_metricsDevice);
     FreeLibrary(m_MDLibraryHandle);
+
     m_metricsDevice = NULL;
     m_MDLibraryHandle = (HMODULE)0;
+    OpenMetricsDevice = NULL;
+    CloseMetricsDevice = NULL;
     return false;
   }
 
