e9d70d7eec744c6d13a3f922cabf1cf1443b43db
Jake Turner <jake@evansturner.co.uk>
2018-12-30 17:04:41 +1300

    osx: enable unit tests for OSX travis building
    force any command failures to call “exit” to make the script fail if a unit test fails

--- util/travis/osx_compile.sh
+++ util/travis/osx_compile.sh
@@ -4,3 +4,9 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j2
+
+echo "--- Running unit tests ---"
+
+trap 'exit' ERR
+./bin/renderdoccmd test -t unit
+./bin/qrenderdoc.app/Contents/MacOS/qrenderdoc --unittest
