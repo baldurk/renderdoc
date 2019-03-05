1a153e9c138890c34875e2e2c63c3972c02b1a8e
baldurk <baldurk@baldurk.org>
2019-03-05 14:01:37 +0000

    Add macOS release build on travis, for deployment

--- util/travis/osx_compile.sh
+++ util/travis/osx_compile.sh
@@ -2,12 +2,20 @@
 
 mkdir build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+if [[ "$RELEASE_BUILD" == "1" ]]; then
+	cmake -DCMAKE_BUILD_TYPE=Release ..
+else
+	cmake -DCMAKE_BUILD_TYPE=Debug ..
+fi
 make -j2
 popd # build
 
 echo "--- Running unit tests ---"
 
-trap 'exit' ERR
-./build/bin/renderdoccmd test unit
-./build/bin/qrenderdoc.app/Contents/MacOS/qrenderdoc --unittest
+if [[ "$RELEASE_BUILD" == "1" ]]; then
+	echo "Not running tests on release build"
+else
+	trap 'exit' ERR
+	./build/bin/renderdoccmd test unit
+	./build/bin/qrenderdoc.app/Contents/MacOS/qrenderdoc --unittest
+fi
