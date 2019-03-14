8652b1a864c1baa17c54145f7cef1d9e2df9f989
Danylo Piliaiev <danylo.piliaiev@globallogic.com>
2019-03-14 15:03:47 +0200

    Fix checking for wrong type in IsDepthAndStencilFormat
    Signed-off-by: Danylo Piliaiev <danylo.piliaiev@globallogic.com>

--- renderdoc/driver/vulkan/vk_resources.cpp
+++ renderdoc/driver/vulkan/vk_resources.cpp
@@ -226,7 +226,7 @@ bool IsDepthAndStencilFormat(VkFormat f)
   switch(f)
   {
     case VK_FORMAT_D16_UNORM_S8_UINT:
-    case VK_FORMAT_X8_D24_UNORM_PACK32:
+    case VK_FORMAT_D24_UNORM_S8_UINT:
     case VK_FORMAT_D32_SFLOAT_S8_UINT: return true;
     default: break;
   }
