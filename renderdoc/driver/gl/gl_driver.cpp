cc065842a5049129dc06f804bc935a623b8e8bf8
Rémi Palandri <rpalandri@fb.com>
2019-11-22 19:32:35 -0800

    add support for serialising invalidate framebuffer calls in GL

--- renderdoc/driver/gl/gl_driver.cpp
+++ renderdoc/driver/gl/gl_driver.cpp
@@ -3687,6 +3687,10 @@ bool WrappedOpenGL::ProcessChunk(ReadSerialiser &ser, GLChunk chunk)
       return Serialise_glFramebufferReadBufferEXT(ser, 0, eGL_NONE);
     case GLChunk::glBindFramebufferEXT:
     case GLChunk::glBindFramebuffer: return Serialise_glBindFramebuffer(ser, eGL_NONE, 0);
+    case GLChunk::glDiscardFramebufferEXT:
+    case GLChunk::glInvalidateFramebuffer:
+    case GLChunk::glInvalidateNamedFramebufferData:
+      return Serialise_glInvalidateNamedFramebufferData(ser, 0, 0, 0);
     case GLChunk::glDrawBuffer:
     case GLChunk::glNamedFramebufferDrawBuffer:
     case GLChunk::glFramebufferDrawBufferEXT:
@@ -4669,11 +4673,8 @@ bool WrappedOpenGL::ProcessChunk(ReadSerialiser &ser, GLChunk chunk)
     case GLChunk::glProgramBinary:
     case GLChunk::glReleaseShaderCompiler:
     case GLChunk::glFrameTerminatorGREMEDY:
-    case GLChunk::glDiscardFramebufferEXT:
     case GLChunk::glInvalidateBufferData:
     case GLChunk::glInvalidateBufferSubData:
-    case GLChunk::glInvalidateFramebuffer:
-    case GLChunk::glInvalidateNamedFramebufferData:
     case GLChunk::glInvalidateNamedFramebufferSubData:
     case GLChunk::glInvalidateSubFramebuffer:
     case GLChunk::glInvalidateTexImage:
