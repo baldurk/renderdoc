baldurk/renderdoc

commit 6d40bbb783ee23eb88995cc097070374970408ed

Add experimental wayland support. Refs #853
* This is only lightly tested and may break heavily. It is disabled by default
 and must be explicitly enabled.
* In particular this is only known to work for Wayland use at capture time.
 Wayland on replay is still unsupported. Known issues include: EGL pbuffer
 surfaces are not implemented on Wayland, Wayland cannot get window dimensions,
 and there are hangs/failures with GL and vulkan presentation with Wayland.

renderdoc/driver/gl/egl_hooks.cpp
@@ -38,9 +38,22 @@
 #if ENABLED(RDOC_LINUX)
 namespace Keyboard
 {
-void CloneDisplay(Display *dpy);
+void UseXlibDisplay(Display *dpy);
+WindowingSystem UseUnknownDisplay(void *disp);
+void UseWaylandDisplay(wl_display *disp);
 }
 #endif
+
+struct SurfaceConfig
+{
+  WindowingSystem system;
+  void *wnd;
+};
+
+struct DisplayConfig
+{
+  WindowingSystem system;
+};
 
 class EGLHook : LibraryHook
 {
@@ -54,7 +67,8 @@ public:
   WrappedOpenGL driver;
   std::set<EGLContext> contexts;
   std::map<EGLContext, EGLConfig> configs;
-  std::map<EGLSurface, EGLNativeWindowType> windows;
+  std::map<EGLSurface, SurfaceConfig> windows;
+  std::map<EGLDisplay, DisplayConfig> displays;
 
   // indicates we're in a swap function, so don't process the swap any further if we recurse - could
   // happen due to driver implementation of one function calling another
@@ -121,12 +135,15 @@ static void EnsureRealLibraryLoaded()
 #if ENABLED(RDOC_LINUX)
   if(eglhook.handle == DEFAULT_HANDLE)
   {
-    RDCLOG("Loading libEGL at the last second");
+    if(!RenderDoc::Inst().IsReplayApp())
+      RDCLOG("Loading libEGL at the last second");
 
     void *handle = Process::LoadModule("libEGL.so");
 
     if(!handle)
       handle = Process::LoadModule("libEGL.so.1");
+
+    eglhook.handle = handle;
   }
 #endif
 }
@@ -144,10 +161,36 @@ HOOK_EXPORT EGLDisplay EGLAPIENTRY eglGetDisplay_renderdoc_hooked(EGLNativeDispl
   EnsureRealLibraryLoaded();
 
 #if ENABLED(RDOC_LINUX)
-  Keyboard::CloneDisplay(display);
+  Keyboard::UseUnknownDisplay((void *)display);
 #endif
 
   return EGL.GetDisplay(display);
+}
+
+HOOK_EXPORT EGLDisplay EGLAPIENTRY eglGetPlatformDisplay_renderdoc_hooked(EGLenum platform,
+                                                                          void *native_display,
+                                                                          const EGLAttrib *attrib_list)
+{
+  if(RenderDoc::Inst().IsReplayApp())
+  {
+    if(!EGL.GetDisplay)
+      EGL.PopulateForReplay();
+
+    return EGL.GetPlatformDisplay(platform, native_display, attrib_list);
+  }
+
+  EnsureRealLibraryLoaded();
+
+#if ENABLED(RDOC_LINUX)
+  if(platform == EGL_PLATFORM_X11_KHR)
+    Keyboard::UseXlibDisplay((Display *)native_display);
+  else if(platform == EGL_PLATFORM_WAYLAND_KHR)
+    Keyboard::UseWaylandDisplay((wl_display *)native_display);
+  else
+    RDCWARN("Unknown platform %x in eglGetPlatformDisplay", platform);
+#endif
+
+  return EGL.GetPlatformDisplay(platform, native_display, attrib_list);
 }
 
 HOOK_EXPORT EGLBoolean EGLAPIENTRY eglBindAPI_renderdoc_hooked(EGLenum api)
@@ -336,7 +379,35 @@ HOOK_EXPORT EGLSurface EGLAPIENTRY eglCreateWindowSurface_renderdoc_hooked(EGLDi
   {
     SCOPED_LOCK(glLock);
 
-    eglhook.windows[ret] = win;
+    // spec says it's implementation dependent what happens, so we assume that we're using the same
+    // window system as the display
+    eglhook.windows[ret] = {eglhook.displays[dpy].system, (void *)win};
+  }
+
+  return ret;
+}
+
+HOOK_EXPORT EGLSurface EGLAPIENTRY eglCreatePlatformWindowSurface_renderdoc_hooked(
+    EGLDisplay dpy, EGLConfig config, void *native_window, const EGLAttrib *attrib_list)
+{
+  if(RenderDoc::Inst().IsReplayApp())
+  {
+    if(!EGL.CreatePlatformWindowSurface)
+      EGL.PopulateForReplay();
+
+    return EGL.CreatePlatformWindowSurface(dpy, config, native_window, attrib_list);
+  }
+
+  EnsureRealLibraryLoaded();
+
+  EGLSurface ret = EGL.CreatePlatformWindowSurface(dpy, config, native_window, attrib_list);
+
+  if(ret)
+  {
+    SCOPED_LOCK(glLock);
+
+    // spec guarantees that we're using the same window system as the display
+    eglhook.windows[ret] = {eglhook.displays[dpy].system, native_window};
   }
 
   return ret;
@@ -376,17 +447,20 @@ HOOK_EXPORT EGLBoolean EGLAPIENTRY eglMakeCurrent_renderdoc_hooked(EGLDisplay di
       GL.DriverForEmulation(&eglhook.driver);
     }
 
+    SurfaceConfig cfg = eglhook.windows[draw];
+
     GLWindowingData data;
     data.egl_dpy = display;
     data.egl_wnd = draw;
     data.egl_ctx = ctx;
-    data.wnd = (decltype(data.wnd))eglhook.windows[draw];
+    data.wnd = (decltype(data.wnd))cfg.wnd;
 
     if(!data.wnd)
     {
       // could be a pbuffer surface or other offscreen rendering. We want a valid wnd, so set it to
       // a dummy value
       data.wnd = (decltype(data.wnd))(void *)(uintptr_t(0xdeadbeef) + uintptr_t(draw));
+      cfg.system = WindowingSystem::Headless;
     }
 
     // we could query this out technically but it's easier to keep a map
@@ -426,7 +500,9 @@ HOOK_EXPORT EGLBoolean EGLAPIENTRY eglSwapBuffers_renderdoc_hooked(EGLDisplay dp
 
     eglhook.RefreshWindowParameters(data);
 
-    eglhook.driver.SwapBuffers(surface);
+    SurfaceConfig cfg = eglhook.windows[surface];
+
+    eglhook.driver.SwapBuffers(cfg.system, cfg.wnd);
   }
 
   {
@@ -456,7 +532,11 @@ HOOK_EXPORT EGLBoolean EGLAPIENTRY eglPostSubBufferNV_renderdoc_hooked(EGLDispla
 
   eglhook.driver.SetDriverType(eglhook.activeAPI);
   if(!eglhook.driver.UsesVRFrameMarkers() && !eglhook.swapping)
-    eglhook.driver.SwapBuffers((void *)eglhook.windows[surface]);
+  {
+    SurfaceConfig cfg = eglhook.windows[surface];
+
+    eglhook.driver.SwapBuffers(cfg.system, cfg.wnd);
+  }
 
   {
     eglhook.swapping = true;
@@ -485,7 +565,11 @@ HOOK_EXPORT EGLBoolean EGLAPIENTRY eglSwapBuffersWithDamageEXT_renderdoc_hooked(
 
   eglhook.driver.SetDriverType(eglhook.activeAPI);
   if(!eglhook.driver.UsesVRFrameMarkers() && !eglhook.swapping)
-    eglhook.driver.SwapBuffers((void *)eglhook.windows[surface]);
+  {
+    SurfaceConfig cfg = eglhook.windows[surface];
+
+    eglhook.driver.SwapBuffers(cfg.system, cfg.wnd);
+  }
 
   {
     eglhook.swapping = true;
@@ -514,7 +598,11 @@ HOOK_EXPORT EGLBoolean EGLAPIENTRY eglSwapBuffersWithDamageKHR_renderdoc_hooked(
 
   eglhook.driver.SetDriverType(eglhook.activeAPI);
   if(!eglhook.driver.UsesVRFrameMarkers() && !eglhook.swapping)
-    eglhook.driver.SwapBuffers((void *)eglhook.windows[surface]);
+  {
+    SurfaceConfig cfg = eglhook.windows[surface];
+
+    eglhook.driver.SwapBuffers(cfg.system, cfg.wnd);
+  }
 
   {
     eglhook.swapping = true;
@@ -549,9 +637,9 @@ eglGetProcAddress_renderdoc_hooked(const char *func)
     return realFunc;
 
 // return our egl hooks
-#define GPA_FUNCTION(name)                                                                          \
-  if(!strcmp(func, "egl" STRINGIZE(name)))                                                          \
-    return (__eglMustCastToProperFunctionPointerType)&CONCAT(egl, CONCAT(name, _renderdoc_hooked)); \
+#define GPA_FUNCTION(name, isext)          \
+  if(!strcmp(func, "egl" STRINGIZE(name))) \
+    return (__eglMustCastToProperFunctionPointerType)&CONCAT(egl, CONCAT(name, _renderdoc_hooked));
   EGL_HOOKED_SYMBOLS(GPA_FUNCTION)
 #undef GPA_FUNCTION
 
@@ -578,6 +666,12 @@ HOOK_EXPORT EGLDisplay EGLAPIENTRY eglGetDisplay(EGLNativeDisplayType display)
   return eglGetDisplay_renderdoc_hooked(display);
 }
 
+HOOK_EXPORT EGLDisplay EGLAPIENTRY eglGetPlatformDisplay(EGLenum platform, void *native_display,
+                                                         const EGLAttrib *attrib_list)
+{
+  return eglGetPlatformDisplay_renderdoc_hooked(platform, native_display, attrib_list);
+}
+
 HOOK_EXPORT EGLContext EGLAPIENTRY eglCreateContext(EGLDisplay display, EGLConfig config,
                                                     EGLContext shareContext, EGLint const *attribList)
 {
@@ -594,6 +688,13 @@ HOOK_EXPORT EGLSurface EGLAPIENTRY eglCreateWindowSurface(EGLDisplay dpy, EGLCon
                                                           const EGLint *attrib_list)
 {
   return eglCreateWindowSurface_renderdoc_hooked(dpy, config, win, attrib_list);
+}
+
+HOOK_EXPORT EGLSurface EGLAPIENTRY eglCreatePlatformWindowSurface(EGLDisplay dpy, EGLConfig config,
+                                                                  void *native_window,
+                                                                  const EGLAttrib *attrib_list)
+{
+  return eglCreatePlatformWindowSurface_renderdoc_hooked(dpy, config, native_window, attrib_list);
 }
 
 HOOK_EXPORT EGLBoolean EGLAPIENTRY eglMakeCurrent(EGLDisplay display, EGLSurface draw,
@@ -757,10 +858,6 @@ EGL_PASSTHRU_4(EGLBoolean, eglGetSyncAttrib, EGLDisplay, dpy, EGLSync, sync, EGL
 EGL_PASSTHRU_5(EGLImage, eglCreateImage, EGLDisplay, dpy, EGLContext, ctx, EGLenum, target,
                EGLClientBuffer, buffer, const EGLAttrib *, attrib_list)
 EGL_PASSTHRU_2(EGLBoolean, eglDestroyImage, EGLDisplay, dpy, EGLImage, image)
-EGL_PASSTHRU_3(EGLDisplay, eglGetPlatformDisplay, EGLenum, platform, void *, native_display,
-               const EGLAttrib *, attrib_list)
-EGL_PASSTHRU_4(EGLSurface, eglCreatePlatformWindowSurface, EGLDisplay, dpy, EGLConfig, config,
-               void *, native_window, const EGLAttrib *, attrib_list)
 EGL_PASSTHRU_4(EGLSurface, eglCreatePlatformPixmapSurface, EGLDisplay, dpy, EGLConfig, config,
                void *, native_pixmap, const EGLAttrib *, attrib_list)
 EGL_PASSTHRU_3(EGLBoolean, eglWaitSync, EGLDisplay, dpy, EGLSync, sync, EGLint, flags)

renderdoc/driver/gl/glx_hooks.cpp
@@ -29,7 +29,7 @@
 
 namespace Keyboard
 {
-void CloneDisplay(Display *dpy);
+void UseXlibDisplay(Display *dpy);
 }
 
 class GLXHook : LibraryHook
@@ -65,18 +65,18 @@ public:
 // callback and we'll get a specific library handle.
 static void EnsureRealLibraryLoaded()
 {
-  if(RenderDoc::Inst().IsReplayApp())
-    return;
-
   if(glxhook.handle == RTLD_NEXT)
   {
-    RDCLOG("Loading libGL at the last second");
+    if(!RenderDoc::Inst().IsReplayApp())
+      RDCLOG("Loading libGL at the last second");
 
     void *handle = Process::LoadModule("libGL.so.1");
     if(!handle)
       handle = Process::LoadModule("libGL.so");
     if(!handle)
       handle = Process::LoadModule("libGLX.so.0");
+
+    glxhook.handle = handle;
   }
 }
 
@@ -106,7 +106,7 @@ HOOK_EXPORT GLXContext glXCreateContext_renderdoc_hooked(Display *dpy, XVisualIn
 
   int value = 0;
 
-  Keyboard::CloneDisplay(dpy);
+  Keyboard::UseXlibDisplay(dpy);
 
   GLX.glXGetConfig(dpy, vis, GLX_BUFFER_SIZE, &value);
   init.colorBits = value;
@@ -251,7 +251,7 @@ HOOK_EXPORT GLXContext glXCreateContextAttribsARB_renderdoc_hooked(Display *dpy,
 
   int value = 0;
 
-  Keyboard::CloneDisplay(dpy);
+  Keyboard::UseXlibDisplay(dpy);
 
   GLX.glXGetConfig(dpy, vis, GLX_BUFFER_SIZE, &value);
   init.colorBits = value;
@@ -444,7 +444,7 @@ HOOK_EXPORT void glXSwapBuffers_renderdoc_hooked(Display *dpy, GLXDrawable drawa
     glxhook.UpdateWindowSize(data, dpy, drawable);
   }
 
-  glxhook.driver.SwapBuffers((void *)drawable);
+  glxhook.driver.SwapBuffers(WindowingSystem::Xlib, (void *)drawable);
 
   GLX.glXSwapBuffers(dpy, drawable);
 }

CMakeLists.txt
@@ -166,6 +166,7 @@ option(ENABLE_PYRENDERDOC "Enable renderdoc python modules" ON)
 
 option(ENABLE_XLIB "Enable xlib windowing support" ON)
 option(ENABLE_XCB "Enable xcb windowing support" ON)
+option(ENABLE_WAYLAND "Enable experimental wayland windowing support" OFF)
 option(ENABLE_GGP "Enable GGP support" OFF)
 
 option(ENABLE_ASAN "Enable address sanitizer" OFF)
@@ -180,6 +181,21 @@ endif()
 if(ENABLE_TSAN)
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread -fno-omit-frame-pointer")
     message(STATUS "Enabling thread sanitizer - may cause issues if capturing, only recommended for use with replay only")
+endif()
+
+if(ENABLE_WAYLAND)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(PKG_WAYLAND QUIET wayland-client)
+
+    if(PKG_WAYLAND_FOUND)
+        if(NOT ENABLE_GLES)
+            message(WARNING "On Wayland GLES support is required for EGL, disabling GL support")
+            set(ENABLE_GL OFF CACHE BOOL "" FORCE)
+        endif()
+    else()
+        message(WARNING "Wayland not found - disabling support")
+        set(ENABLE_WAYLAND OFF CACHE BOOL "" FORCE)
+    endif()
 endif()
 
 if(WIN32)
@@ -328,6 +344,10 @@ elseif(UNIX)
     if(ENABLE_XCB)
         add_definitions(-DRENDERDOC_WINDOWING_XCB)
     endif()
+
+    if(ENABLE_WAYLAND)
+        add_definitions(-DRENDERDOC_WINDOWING_WAYLAND)
+    endif()
 endif()
 
 add_subdirectory(renderdoc)
@@ -378,6 +398,10 @@ if(UNIX AND NOT ANDROID AND NOT APPLE AND NOT ENABLE_GGP)
 
     if(ENABLE_XCB)
         message(STATUS "  - XCB")
+    endif()
+
+    if(ENABLE_WAYLAND)
+        message(STATUS "  - Wayland")
     endif()
 endif()
 

qrenderdoc/CMakeLists.txt
@@ -168,6 +168,13 @@ if(CMAKE_COMPILER_IS_GNUCXX)
         "QMAKE_CXXFLAGS+=-Wno-unknown-warning -Wno-implicit-fallthrough -Wno-cast-function-type -Wno-stringop-truncation\n")
 endif()
 
+if(ENABLE_WAYLAND)
+    message(WARNING "!!!! Using the Wayland Qt platform in qrenderdoc is NOT SUPPORTED !!!!")
+    file(APPEND
+        ${CMAKE_BINARY_DIR}/qrenderdoc/qrenderdoc_cmake.pri
+        "DEFINES+=RENDERDOC_WINDOWING_WAYLAND\n")
+endif()
+
 # propagate build version info. Lots of escaping needed here to pass ""s into the define value
 if(BUILD_VERSION_STABLE)
     file(APPEND

qrenderdoc/Code/CaptureContext.cpp
@@ -849,22 +849,48 @@ void CaptureContext::LoadCaptureThreaded(const QString &captureFile, const Repla
 #if defined(RENDERDOC_PLATFORM_WIN32)
     m_CurWinSystem = WindowingSystem::Win32;
 #elif defined(RENDERDOC_PLATFORM_LINUX)
-    m_CurWinSystem = WindowingSystem::Xlib;
+    m_CurWinSystem = WindowingSystem::Unknown;
 
-    // prefer XCB, if supported
-    for(WindowingSystem sys : m_WinSystems)
+    if(QGuiApplication::platformName() == lit("wayland"))
     {
-      if(sys == WindowingSystem::XCB)
+      // if we're using Wayland we must use wayland since we can't get any other surface type
+      for(WindowingSystem sys : m_WinSystems)
       {
-        m_CurWinSystem = WindowingSystem::XCB;
-        break;
+        if(sys == WindowingSystem::Wayland)
+        {
+          m_CurWinSystem = WindowingSystem::Wayland;
+          m_WaylandDisplay = (wl_display *)AccessWaylandPlatformInterface("display", NULL);
+          break;
+        }
+      }
+
+      if(m_CurWinSystem == WindowingSystem::Unknown)
+      {
+        RDDialog::critical(NULL, tr("No wayland support"),
+                           tr("Replay doesn't support Wayland surfaces - check you compiled this "
+                              "build of RenderDoc with Wayland support enabled."));
       }
     }
-
-    if(m_CurWinSystem == WindowingSystem::XCB)
-      m_XCBConnection = QX11Info::connection();
     else
-      m_X11Display = QX11Info::display();
+    {
+      m_CurWinSystem = WindowingSystem::Xlib;
+
+      // prefer XCB, if supported
+      for(WindowingSystem sys : m_WinSystems)
+      {
+        if(sys == WindowingSystem::XCB)
+        {
+          m_CurWinSystem = WindowingSystem::XCB;
+          break;
+        }
+      }
+
+      if(m_CurWinSystem == WindowingSystem::XCB)
+        m_XCBConnection = QX11Info::connection();
+      else
+        m_X11Display = QX11Info::display();
+    }
+
 #elif defined(RENDERDOC_PLATFORM_APPLE)
     m_CurWinSystem = WindowingSystem::MacOS;
 #endif
@@ -1777,10 +1803,26 @@ WindowingData CaptureContext::CreateWindowingData(QWidget *window)
 
 #elif defined(RENDERDOC_PLATFORM_LINUX)
 
-  if(m_CurWinSystem == WindowingSystem::XCB)
+  if(m_CurWinSystem == WindowingSystem::Wayland)
+  {
+    // we don't need this, we just need to force creation of a window handle
+    window->winId();
+    wl_surface *surface =
+        (wl_surface *)AccessWaylandPlatformInterface("surface", window->windowHandle());
+    return CreateWaylandWindowingData(m_WaylandDisplay, surface);
+  }
+  else if(m_CurWinSystem == WindowingSystem::XCB)
+  {
     return CreateXCBWindowingData(m_XCBConnection, (xcb_window_t)window->winId());
-  else
+  }
+  else if(m_CurWinSystem == WindowingSystem::Xlib)
+  {
     return CreateXlibWindowingData(m_X11Display, (Drawable)window->winId());
+  }
+  else
+  {
+    return CreateHeadlessWindowingData(1, 1);
+  }
 
 #elif defined(RENDERDOC_PLATFORM_APPLE)
 

qrenderdoc/Code/CaptureContext.h
@@ -367,6 +367,7 @@ private:
   WindowingSystem m_CurWinSystem = WindowingSystem::Unknown;
 
 #if defined(RENDERDOC_PLATFORM_LINUX)
+  wl_display *m_WaylandDisplay = NULL;
   xcb_connection_t *m_XCBConnection = NULL;
   Display *m_X11Display = NULL;
 #endif

qrenderdoc/Code/QRDUtils.cpp
@@ -2156,3 +2156,28 @@ QColor contrastingColor(const QColor &col, const QColor &defaultCol)
   else
     return QColor(Qt::black);
 }
+
+// we declare this partial class to get the accessors. THIS IS DANGEROUS as the ABI is unstable and
+// this is a private class. The first few functions have been stable for a while so we hope that it
+// will remain so. If a stable interface is added in future like QX11Info we should definitely use
+// it instead.
+//
+// Unfortunately we need this for Wayland, so we only ever use it when we are absolutely forced to
+// because we're running under the Wayland Qt platform.
+class QOpenGLContext;
+
+class Q_GUI_EXPORT QPlatformNativeInterface : public QObject
+{
+  Q_OBJECT
+public:
+  virtual void *nativeResourceForIntegration(const QByteArray &resource);
+  virtual void *nativeResourceForContext(const QByteArray &resource, QOpenGLContext *context);
+  virtual void *nativeResourceForScreen(const QByteArray &resource, QScreen *screen);
+  virtual void *nativeResourceForWindow(const QByteArray &resource, QWindow *window);
+};
+
+void *AccessWaylandPlatformInterface(const QByteArray &resource, QWindow *window)
+{
+  QPlatformNativeInterface *native = QGuiApplication::platformNativeInterface();
+  return native->nativeResourceForWindow(resource, window);
+}

qrenderdoc/Code/QRDUtils.h
@@ -609,3 +609,5 @@ bool IsDarkTheme();
 
 float getLuminance(const QColor &col);
 QColor contrastingColor(const QColor &col, const QColor &defaultCol);
+
+void *AccessWaylandPlatformInterface(const QByteArray &resource, QWindow *window);

commit e345a3ff976880b5fe2c40fe95af3537a8af2974

Fix a handling of dlsym(RTLD_NEXT) without loading libGL. Closes #1463
* The original fix leads to infinite recursion in GLX, EGL hooks.
  Due to `dlopen` function override in `librenderdoc.so` library the
  `Process::LoadModule` function always returns a handle of
  `librenderdoc.so` instead of requested library. So instead of
  `real function` we receive an address of the `hook` that leads
  to the infinite recursion in this `hook`.

Fixes: 6d40bbb ('Add experimental wayland support. Refs #853')
Tested-by: Andrii Simiklit <andrii.simiklit@globallogic.com>

renderdoc/driver/gl/egl_hooks.cpp
@@ -143,7 +143,8 @@ static void EnsureRealLibraryLoaded()
     if(!handle)
       handle = Process::LoadModule("libEGL.so.1");
 
-    eglhook.handle = handle;
+    if(RenderDoc::Inst().IsReplayApp())
+      eglhook.handle = handle;
   }
 #endif
 }

renderdoc/driver/gl/glx_hooks.cpp
@@ -76,7 +76,8 @@ static void EnsureRealLibraryLoaded()
     if(!handle)
       handle = Process::LoadModule("libGLX.so.0");
 
-    glxhook.handle = handle;
+    if(RenderDoc::Inst().IsReplayApp())
+      glxhook.handle = handle;
   }
 }
 