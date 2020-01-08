5255db7966ad0ad799ab40df3e8941c47007bfb7
baldurk <baldurk@baldurk.org>
2020-01-08 19:15:52 +0000

    Handle tkinter not importing and disable relevant test. Closes #1668

--- util/test/tests/D3D12/D3D12_RGP_Capture.py
+++ util/test/tests/D3D12/D3D12_RGP_Capture.py
@@ -2,9 +2,19 @@ import renderdoc as rd
 import rdtest
 import os
 
+try:
+    import tkinter
+except ImportError as ex:
+    tkinter = None
 
 class D3D12_RGP_Capture(rdtest.TestCase):
     demos_test_name = 'D3D12_Simple_Triangle'
+
+    def check_support(self):
+        if tkinter is None:
+            return False, 'tkinter is required but not available'
+        
+        return super().check_support()
 
     # Need to enable RGP mode before opening the capture
     def run(self):
@@ -18,7 +28,6 @@ class D3D12_RGP_Capture(rdtest.TestCase):
             rdtest.log.print("RGP capture not tested")
             return
 
-        import tkinter
 
         # On D3D12 we need to create a real window
         window = tkinter.Tk()
