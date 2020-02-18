454d03a28ec5d037fb138005f50c0d68e3f7a764
baldurk <baldurk@baldurk.org>
2020-02-18 20:47:57 +0000

    Fix tautological compare

--- renderdoc/driver/shaders/dxbc/dxbc_disassemble.cpp
+++ renderdoc/driver/shaders/dxbc/dxbc_disassemble.cpp
@@ -287,7 +287,7 @@ bool Operand::sameResource(const Operand &o) const
   if(indices.size() < 1 || o.indices.size() < 1)
     return false;
 
-  return indices[0] == indices[0];
+  return indices[0] == o.indices[0];
 }
 
 void Program::FetchTypeVersion()
