85a57525698a12413bbbc9e762d0162728fabebe
baldurk <baldurk@baldurk.org>
2020-03-06 10:08:06 +0000

    Fix use of check_pixel_value in GL_Unshared_Context test

--- util/test/tests/GL/GL_Unshared_Context.py
+++ util/test/tests/GL/GL_Unshared_Context.py
@@ -21,11 +21,11 @@ class GL_Unshared_Context(rdtest.TestCase):
         magic_value: rd.PixelValue = [1.0, 0.5, 0.25, 1.0]
         epsilon = .005
         
-        self.check_pixel_value(id, 0.0, 0.0, magic_value, epsilon)
-        self.check_pixel_value(id, 1.0, 0.0, magic_value, epsilon)
-        self.check_pixel_value(id, 0.0, 1.0, magic_value, epsilon)
-        self.check_pixel_value(id, 1.0, 1.0, magic_value, epsilon)
-        self.check_pixel_value(id, 0.5, 0.5, magic_value, epsilon)
+        self.check_pixel_value(id, 0.0, 0.0, magic_value, eps=epsilon)
+        self.check_pixel_value(id, 1.0, 0.0, magic_value, eps=epsilon)
+        self.check_pixel_value(id, 0.0, 1.0, magic_value, eps=epsilon)
+        self.check_pixel_value(id, 1.0, 1.0, magic_value, eps=epsilon)
+        self.check_pixel_value(id, 0.5, 0.5, magic_value, eps=epsilon)
         
         rdtest.log.success("Texture captured properly from unshared context")
 
