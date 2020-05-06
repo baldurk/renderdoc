9d67549407fe4c7b8ad19586bfe9e8358dafd1b3
baldurk <baldurk@baldurk.org>
2020-05-06 12:44:10 +0100

    When enabling API validation register khronos validation layer too

--- renderdoc/driver/vulkan/vk_layer.cpp
+++ renderdoc/driver/vulkan/vk_layer.cpp
@@ -143,6 +143,10 @@ class VulkanHook : LibraryHook
       Process::RegisterEnvironmentModification(
           EnvironmentModification(EnvMod::Append, EnvSep::Platform, "VK_DEVICE_LAYERS",
                                   "VK_LAYER_LUNARG_standard_validation"));
+      Process::RegisterEnvironmentModification(EnvironmentModification(
+          EnvMod::Append, EnvSep::Platform, "VK_INSTANCE_LAYERS", "VK_LAYER_KHRONOS_validation"));
+      Process::RegisterEnvironmentModification(EnvironmentModification(
+          EnvMod::Append, EnvSep::Platform, "VK_DEVICE_LAYERS", "VK_LAYER_KHRONOS_validation"));
     }
     else
     {
