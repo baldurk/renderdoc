8f2d7f91f29724a4378a0458138f5dfa871e60fd
baldurk <baldurk@baldurk.org>
2020-05-20 12:27:24 +0100

    Drop const as well since some compilers warn that it's redundant

--- renderdoc/core/settings.h
+++ renderdoc/core/settings.h
@@ -66,7 +66,7 @@ CONFIG_SUPPORT_TYPE(rdcarray<rdcstr>);
 #define RDOC_DEBUG_CONFIG(type, name, defaultValue, description)                         \
   static ConfigVarRegistration<type> CONCAT(config, __LINE__)(                           \
       STRING_LITERAL(STRINGIZE(name)), defaultValue, true, STRING_LITERAL(description)); \
-  static const type name() { return defaultValue; }
+  static type name() { return defaultValue; }
 #else
 
 #define RDOC_DEBUG_CONFIG(type, name, defaultValue, description)                         \
