6e899b894d17383cdb66071f3cff4212e4525b70
baldurk <baldurk@baldurk.org>
2020-06-11 17:37:16 +0100

    Disable warning in tinyexr with latest VS2019 compiler

--- renderdoc/3rdparty/tinyexr/tinyexr.cpp
+++ renderdoc/3rdparty/tinyexr/tinyexr.cpp
@@ -3,5 +3,9 @@
 #include <time.h>
 #include "miniz/miniz.h"
 
+#pragma warning(disable : 5208)    // unnamed class used in typedef name cannot declare members
+                                   // other than non-static data members, member enumerations, or
+                                   // member classes
+
 #define TINYEXR_IMPLEMENTATION
 #include "tinyexr.h"
