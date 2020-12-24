ae03116c5e58d58066a6a1657d5624d8c75ada5b
Kanglai Qian <qiankanglai@corp.netease.com>
2020-12-24 16:31:00 +0800

    supporting full glCopyImageSubData

--- renderdoc/driver/gl/gl_resources.h
+++ renderdoc/driver/gl/gl_resources.h
@@ -29,6 +29,7 @@
 #include "gl_common.h"
 
 size_t GetCompressedByteSize(GLsizei w, GLsizei h, GLsizei d, GLenum internalformat);
+rdcpair<uint32_t, uint32_t> GetCompressedBlockSize(GLenum internalformat);
 size_t GetByteSize(GLsizei w, GLsizei h, GLsizei d, GLenum format, GLenum type);
 GLenum GetBaseFormat(GLenum internalFormat);
 GLenum GetDataType(GLenum internalFormat);
