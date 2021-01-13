026da176bbcc1678c439984cbbea1c17953c3a0e
baldurk <baldurk@baldurk.org>
2021-01-13 13:56:10 +0000

    Update copyright years to 2021

--- renderdoc/driver/d3d9/d3d9_hooks.cpp
+++ renderdoc/driver/d3d9/d3d9_hooks.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  * The MIT License (MIT)
  *
- * * Copyright (c) 2019-2020 Baldur Karlsson
+ * Copyright (c) 2019-2021 Baldur Karlsson
  * Copyright (c) 2014 Crytek
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
