baldurk/renderdoc

commit fa674a59b6207ee1d0124010dd924580b6f8356d

Consistently use outlineBrush() for drawing borders

qrenderdoc/Styles/RDStyle/RDStyle.cpp
@@ -1630,7 +1630,7 @@ void RDStyle::drawControl(ControlElement control, const QStyleOption *opt, QPain
     qreal lineWidth = qMax(1, frame->lineWidth);
 
     p->save();
-    p->setPen(QPen(opt->palette.brush(widget->foregroundRole()), lineWidth));
+    p->setPen(QPen(outlineBrush(opt->palette), lineWidth));
 
     qreal adjust = 0.5 * lineWidth;
 

commit c6be4f7834f6b27d8c594bc21b9e7a9678830a81

Fix highlighting of active thumbnail preview
* This was broken in fa674a59b6207ee1d0124010dd924580b6f8356d where the style
  overrode the widget's foreground role. Changed to only use the custom
  light/dark role when the desired role is Text (or WindowText).

qrenderdoc/Styles/RDStyle/RDStyle.cpp
@@ -1468,9 +1468,12 @@ void RDStyle::drawPrimitive(PrimitiveElement element, const QStyleOption *opt, Q
   RDTweakedNativeStyle::drawPrimitive(element, opt, p, widget);
 }
 
-const QBrush &RDStyle::outlineBrush(const QPalette &pal) const
+const QBrush &RDStyle::outlineBrush(const QPalette &pal, QPalette::ColorRole role) const
 {
-  return m_Scheme == Light ? pal.brush(QPalette::WindowText) : pal.brush(QPalette::Light);
+  if(role == QPalette::Text || role == QPalette::WindowText)
+    return m_Scheme == Light ? pal.brush(QPalette::WindowText) : pal.brush(QPalette::Light);
+
+  return pal.brush(role);
 }
 
 void RDStyle::drawControl(ControlElement control, const QStyleOption *opt, QPainter *p,
@@ -1630,7 +1633,8 @@ void RDStyle::drawControl(ControlElement control, const QStyleOption *opt, QPain
     qreal lineWidth = qMax(1, frame->lineWidth);
 
     p->save();
-    p->setPen(QPen(outlineBrush(opt->palette), lineWidth));
+
+    p->setPen(QPen(outlineBrush(opt->palette, widget->foregroundRole()), lineWidth));
 
     qreal adjust = 0.5 * lineWidth;
 

qrenderdoc/Styles/RDStyle/RDStyle.h
@@ -98,7 +98,8 @@ protected:
 
   bool eventFilter(QObject *watched, QEvent *event) override;
 
-  const QBrush &outlineBrush(const QPalette &pal) const;
+  const QBrush &outlineBrush(const QPalette &pal,
+                             QPalette::ColorRole role = QPalette::Foreground) const;
 
   void drawRoundedRectBorder(const QStyleOption *opt, QPainter *p, const QWidget *widget,
                              QPalette::ColorRole fillRole, bool shadow) const;

qrenderdoc/Widgets/ResourcePreview.cpp
@@ -36,7 +36,13 @@ ResourcePreview::ResourcePreview(ICaptureContext &c, IReplayOutput *output, QWid
   ui->thumbnail->SetOutput(output);
 
   setBackgroundRole(QPalette::Background);
-  setForegroundRole(QPalette::Highlight);
+  setForegroundRole(QPalette::Foreground);
+
+  QPalette Pal(palette());
+
+  Pal.setColor(QPalette::Highlight, QColor(Qt::red));
+
+  setPalette(Pal);
 
   setSelected(false);
 
@@ -99,17 +105,7 @@ void ResourcePreview::setSelected(bool sel)
 {
   m_Selected = sel;
 
-  QPalette Pal(palette());
-
-  Pal.setColor(QPalette::Highlight, sel ? QColor(Qt::red) : Pal.color(QPalette::Foreground));
-
-  setPalette(Pal);
-}
-
-void ResourcePreview::changeEvent(QEvent *event)
-{
-  if(event->type() == QEvent::PaletteChange || event->type() == QEvent::StyleChange)
-    setSelected(m_Selected);
+  setForegroundRole(sel ? QPalette::Highlight : QPalette::Foreground);
 }
 
 WindowingData ResourcePreview::GetWidgetWindowingData()

qrenderdoc/Widgets/ResourcePreview.h
@@ -67,9 +67,6 @@ public:
 
   void setSelected(bool sel);
 
-protected:
-  void changeEvent(QEvent *event) override;
-
 private:
   Ui::ResourcePreview *ui;
 