6d7d4829ee70e1e84b9ff91bd679bba92a090f3c
baldurk <baldurk@baldurk.org>
2022-05-30 17:09:17 +0100

    Fix docs build on sphinx 5

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ release = 'v{0}'.format(version)
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
