34b6f473626cebf5ff4d6dc67a1803055b0b6fcb
Jake Turner <jake@evansturner.co.uk>
2022-08-12 06:10:19 +0100

    Metal Replay resource creation helper methods
    Added WrappedMTLDevice
    void AddResource(ResourceId id, ResourceType type, const char *defaultNamePrefix);
    void DerivedResource(ResourceId parentLive, ResourceId child);
    template <typename MetalType> void DerivedResource(MetalType parent, ResourceId child);
    
    Start of MetalReplay implementation to support replay resource creation:
    ResourceDescription &GetResourceDesc(ResourceId id);
    
    MetalReplay instance is not created in this commit

---
+++ renderdoc/driver/metal/metal_replay.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Baldur Karlsson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#include "metal_replay.h"
+
+MetalReplay::MetalReplay(WrappedMTLDevice *wrappedMTLDevice)
+{
+  m_pDriver = wrappedMTLDevice;
+}
+
+ResourceDescription &MetalReplay::GetResourceDesc(ResourceId id)
+{
+  auto it = m_ResourceIdx.find(id);
+  if(it == m_ResourceIdx.end())
+  {
+    m_ResourceIdx[id] = m_Resources.size();
+    m_Resources.push_back(ResourceDescription());
+    m_Resources.back().resourceId = id;
+    return m_Resources.back();
+  }
+
+  return m_Resources[it->second];
+}
