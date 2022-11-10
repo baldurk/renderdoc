c0b931eb15d1f5fcc29cfc068bea0ee554108deb
Bruce He <zm2he@uwaterloo.ca>
2022-11-09 22:18:05 -0500

    Improved fragment id shader code
    - Deleted WriteClearValues and replace with QueryPostModValues
    - Add support for earlier GLSL versions and fix logic for creating primitive id program
    - Changed to only run per fragment queries on events with multiple fragments

--- renderdoc/data/resource.h
+++ renderdoc/data/resource.h
@@ -65,6 +65,7 @@
 #define RESOURCE_glsl_vk_depthms2buffer_comp         448
 #define RESOURCE_glsl_vk_buffer2ms_comp              449
 #define RESOURCE_glsl_vk_depthbuf2ms_frag		     450
+#define RESOURCE_glsl_pixelhistory_primid_legacy_frag 451
 
 // Next default values for new objects
 // 
