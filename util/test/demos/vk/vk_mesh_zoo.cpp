7ba4565d9be132524ec6f4020cf0f9d7ec67f195
hsm <hsm@anubis.rocks>
2022-12-09 18:24:26 -0500

    Replace NULL macro with VK_NULL_HANDLE

--- util/test/demos/vk/vk_mesh_zoo.cpp
+++ util/test/demos/vk/vk_mesh_zoo.cpp
@@ -336,7 +336,7 @@ void main()
 
     VkPipeline stride0pipe = createGraphicsPipeline(pipeCreateInfo);
 
-    VkPipeline xfbpipe = NULL;
+    VkPipeline xfbpipe = VK_NULL_HANDLE;
 
     VkPipelineRasterizationStateStreamCreateInfoEXT rastInfo = {
         VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_STREAM_CREATE_INFO_EXT, NULL, 0, 2,
