c3266acd15a7c621381ca01045933588565a08e6
baldurk <baldurk@baldurk.org>
2023-06-22 12:21:33 +0100

    Remove D3D8 and D3D9 stub backends
    * These were added 6-7 years ago and have been untouched since. They add no
      value to preserve as they are almost guaranteed to never be reasonably
      implemented.
    * The only thing that is useful is the D3D9 hooks for begin/end events used in
      some D3D11 programs, which has been moved to the D3D11 project.

--- renderdoc/driver/d3d11/d3d9_hooks.cpp
+++ renderdoc/driver/d3d11/d3d9_hooks.cpp
@@ -26,16 +26,11 @@
 #include "driver/d3d11/d3d11_device.h"
 #include "hooks/hooks.h"
 
-#include "driver/dx/official/d3d9.h"
-#include "d3d9_device.h"
-
 typedef int(WINAPI *PFN_BEGIN_EVENT)(DWORD, WCHAR *);
 typedef int(WINAPI *PFN_END_EVENT)();
 typedef int(WINAPI *PFN_SET_MARKER_EVENT)(DWORD, WCHAR *);
 typedef void(WINAPI *PFN_SET_OPTIONS)(DWORD);
 typedef DWORD(WINAPI *PFN_GET_OPTIONS)();
-
-typedef IDirect3D9 *(WINAPI *PFN_D3D9_CREATE)(UINT);
 
 class D3D9Hook : LibraryHook
 {
@@ -51,8 +46,6 @@ public:
     PERF_SetMarker.Register("d3d9.dll", "D3DPERF_SetMarker", PERF_SetMarker_hook);
     PERF_SetOptions.Register("d3d9.dll", "D3DPERF_SetOptions", PERF_SetOptions_hook);
     PERF_GetStatus.Register("d3d9.dll", "D3DPERF_GetStatus", PERF_GetStatus_hook);
-
-    Create9.Register("d3d9.dll", "Direct3DCreate9", Create9_hook);
   }
 
 private:
@@ -64,7 +57,6 @@ private:
   HookedFunction<PFN_SET_MARKER_EVENT> PERF_SetMarker;
   HookedFunction<PFN_SET_OPTIONS> PERF_SetOptions;
   HookedFunction<PFN_GET_OPTIONS> PERF_GetStatus;
-  HookedFunction<PFN_D3D9_CREATE> Create9;
 
   static int WINAPI PERF_BeginEvent_hook(DWORD col, WCHAR *wszName)
   {
@@ -98,14 +90,6 @@ private:
   }
 
   static DWORD WINAPI PERF_GetStatus_hook() { return 1; }
-  static IDirect3D9 *WINAPI Create9_hook(UINT SDKVersion)
-  {
-    RDCLOG("App creating d3d9 %x", SDKVersion);
-
-    IDirect3D9 *realD3D = d3d9hooks.Create9()(SDKVersion);
-
-    return new WrappedD3D9(realD3D);
-  }
 };
 
 D3D9Hook D3D9Hook::d3d9hooks;
