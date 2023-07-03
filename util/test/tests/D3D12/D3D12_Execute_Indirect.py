9545266d209eb8afe9b5d0e69aa9ca18e0262025
Jake Turner <jake@evansturner.co.uk>
2023-07-03 11:54:49 +0100

    Extend D3D12 Execute Indirect test
    Change it to do 8 indirect draws with a clear in between.
    Check the pixel and mesh data for each draw is correct (simple triangle)

--- util/test/tests/D3D12/D3D12_Execute_Indirect.py
+++ util/test/tests/D3D12/D3D12_Execute_Indirect.py
@@ -6,9 +6,37 @@ class D3D12_Execute_Indirect(rdtest.TestCase):
     demos_test_name = 'D3D12_Execute_Indirect'
 
     def check_capture(self):
-        action = self.find_action("IndirectDraw")
+        from_eid = 0
+        for i in range(8):
+            action = self.find_action("IndirectDraw", from_eid)
+            self.controller.SetFrameEvent(action.eventId, False)
+            # Should be a green triangle in the centre of the screen on a red background
+            self.check_triangle(back=[1.0, 0.0, 0.0, 1.0])
+            postvs_data = self.get_postvs(action, rd.MeshDataStage.VSOut, 0, action.numIndices)
 
-        self.controller.SetFrameEvent(action.eventId, False)
+            postvs_ref = {
+            0: {
+            'vtx': 0,
+            'idx': 0,
+            'SV_POSITION': [-0.5, -0.5, 0.0, 1.0],
+            'COLOR': [0.0, 1.0, 0.0, 1.0],
+            'TEXCOORD': [0.0, 0.0],
+            },
+            1: {
+            'vtx': 1,
+            'idx': 1,
+            'SV_POSITION': [0.0, 0.5, 0.0, 1.0],
+            'COLOR': [0.0, 1.0, 0.0, 1.0],
+            'TEXCOORD': [0.0, 1.0],
+            },
+            2: {
+            'vtx': 2,
+            'idx': 2,
+            'SV_POSITION': [0.5, -0.5, 0.0, 1.0],
+            'COLOR': [0.0, 1.0, 0.0, 1.0],
+            'TEXCOORD': [1.0, 0.0],
+            },
+            }
 
-        # Should be a green triangle in the centre of the screen on a red background
-        self.check_triangle(back=[1.0, 0.0, 0.0, 1.0])
+            self.check_mesh_data(postvs_ref, postvs_data)
+            from_eid = action.eventId + 1
