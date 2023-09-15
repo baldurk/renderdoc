d88eff15f3c3e919c45c86dd6374ce3ed43cc823
baldurk <baldurk@baldurk.org>
2023-09-15 17:51:31 +0100

    Update execute indirect test to verify more complex EI behaviour

--- util/test/demos/d3d12/d3d12_execute_indirect.cpp
+++ util/test/demos/d3d12/d3d12_execute_indirect.cpp
@@ -29,81 +29,430 @@ RD_TEST(D3D12_Execute_Indirect, D3D12GraphicsTest)
   static constexpr const char *Description =
       "Tests use of ExecuteIndirect() in different edge-case scenarios.";
 
+  std::string vert = R"EOSHADER(
+
+struct vertin
+{
+  float4 pos : POSITION;
+  float4 col : COLOR0;
+};
+
+struct v2f
+{
+  float4 pos : SV_POSITION;
+  float4 col : COLOR0;
+};
+
+cbuffer test : register(b0)
+{
+   float4 cbtest[5];
+}
+
+StructuredBuffer<float4> srvtest : register(t0);
+
+RWStructuredBuffer<float4> uavtest : register(u0);
+
+v2f main(vertin IN, uint vid : SV_VertexID)
+{
+  v2f OUT = (v2f)0;
+
+  if(vid < 3)
+  {
+    OUT.pos = float4(IN.pos.xyz, 1);
+    OUT.col = IN.col;
+
+    if(cbtest[1].w != 1.234f)
+      OUT.col.r += 0.1f;
+
+    if(srvtest[1].w != 1.234f)
+      OUT.col.r += 0.2f;
+
+    if(uavtest[1].w != 1.234f)
+      OUT.col.r += 0.5f;
+  }
+  else
+  {
+    float4 positions[] = {
+      float4(-0.5f, -0.5f, 0.0f, 1.0f),
+      float4( 0.0f,  0.5f, 0.0f, 1.0f),
+      float4( 0.5f, -0.5f, 0.0f, 1.0f),
+    };
+
+    OUT.pos = positions[vid-3];
+    OUT.pos.x += 0.5f;
+    OUT.col = float4(1,0,1,1);
+  }
+
+  return OUT;
+}
+
+)EOSHADER";
+
+  std::string vert2 = R"EOSHADER(
+
+struct vertin
+{
+  float4 pos : POSITION;
+  float4 col : COLOR0;
+};
+
+struct v2f
+{
+  float4 pos : SV_POSITION;
+  float4 col : COLOR0;
+};
+
+v2f main(vertin IN, uint vid : SV_VertexID)
+{
+  v2f OUT = (v2f)0;
+
+  OUT.pos = float4(IN.pos.xyz, 1);
+  OUT.col = IN.col;
+
+  return OUT;
+}
+
+)EOSHADER";
+
+  std::string pixel = R"EOSHADER(
+
+struct v2f
+{
+  float4 pos : SV_POSITION;
+  float4 col : COLOR0;
+};
+
+float4 main(v2f IN) : SV_Target0
+{
+  return IN.col;
+}
+
+)EOSHADER";
+
+  std::string comp = R"EOSHADER(
+
+RWStructuredBuffer<float4> bufout : register(u0);
+
+RWByteAddressBuffer customvbargs : register(u1);
+
+[numthreads(1,1,1)]
+void main(uint3 gid : SV_GroupID)
+{
+  uint tid = gid.z*30*12 + gid.y*12 + gid.x;
+
+  bufout[tid] = float4(gid, tid);
+
+  // try to pick some threads that will race to fill in the draw parameters
+
+  // ignore the first set of threadgroups
+  if(tid < 300)
+    return;
+
+  tid -= 300;
+
+  // pick one threadgroup out of every 128
+  if((tid % 128) != 17)
+    return;
+  tid /= 128;
+
+  // now pick the first 8
+  if(tid >= 8)
+    return;
+
+  const uint drawStride = 16;
+  const uint vertStride = 32;
+  const uint paddingVerts = 15;
+  const uint vbStart = 256;
+
+  uint numVerts = 3*(1+tid);
+  uint startVtx = 100*tid + 5;
+
+  uint drawslot;
+  customvbargs.InterlockedAdd(0, 1, drawslot);
+
+  uint vbdataslot;
+  customvbargs.InterlockedAdd(4, vertStride*(numVerts+paddingVerts*2), vbdataslot);
+
+  customvbargs.Store4((1+drawslot)*drawStride, uint4(numVerts, 1, (vbdataslot / vertStride) + 15, vbdataslot));
+
+  // first fill our range with invalid vertices that will show up
+  for(uint vert = 0; vert < numVerts+paddingVerts*2; vert++)
+  {
+    float2 pos;
+    switch(vert % 4)
+    {
+      default:
+      case 0:
+        pos.x = 1100.0f; pos.y = 0.6f; break;
+      case 1:
+        pos.x = -1200.0f; pos.y = 0.2f; break;
+      case 2:
+        pos.x = 1300.0f; pos.y = -0.2f; break;
+      case 3:
+        pos.x = -1400.0f; pos.y = -0.6f; break;
+    }
+
+    customvbargs.Store4(vbStart + vbdataslot + vert*vertStride, asuint(float4(pos.x, pos.y, vert, 0)));
+    customvbargs.Store4(vbStart + vbdataslot + vert*vertStride + 16, asuint(float4(1.0f, 0.0f, 1.0f, 1.0f)));
+  }
+
+  // skip the 'middle' in the 9 space
+  if(tid >= 4) tid++;
+  
+  float2 origin = float2(float(tid%3)/2.0f, 1.0f - float(tid/3)/2.0f);
+
+  // squeeze in a bit towards the centre
+  origin = ((origin * 2.0f - 1.0f.xx) * 0.75f.xx);
+
+  float2 pos[24];
+  for(int tri=0; tri < 8; tri++)
+  {
+    float x = (float(tri)/8.0f)*300.0f;
+
+    pos[tri*3+0] = float2(0.0f, 0.0f);
+    pos[tri*3+1] = float2(sin(radians(x)), cos(radians(x)));
+    pos[tri*3+2] = float2(sin(radians(x+30.0f)), cos(radians(x+30.0f)));
+  }
+
+  // now fill in just the correct vertices
+  for(uint i=0; i < numVerts; i++)
+  {
+    customvbargs.Store4(vbStart + vbdataslot + (paddingVerts + i)*vertStride, asuint(float4(origin.x+pos[i].x*0.2f, origin.y+pos[i].y*0.2f, 0, 0)));
+    customvbargs.Store4(vbStart + vbdataslot + (paddingVerts + i)*vertStride + 16, asuint(float4(0.0f, 1.0f, 0.0f, 1.0f)));
+  }
+}
+
+)EOSHADER";
+
   int main()
   {
     // initialise, create window, create device, etc
     if(!Init())
       return 3;
 
-    ID3DBlobPtr vsblob = Compile(D3DDefaultVertex, "main", "vs_4_0");
-    ID3DBlobPtr psblob = Compile(D3DDefaultPixel, "main", "ps_4_0");
+    ID3DBlobPtr vsblob = Compile(vert, "main", "vs_5_0");
+    ID3DBlobPtr vs2blob = Compile(vert2, "main", "vs_5_0");
+    ID3DBlobPtr psblob = Compile(pixel, "main", "ps_4_0");
 
-    const DefaultA2V tri[3] = {
-        {Vec3f(-0.5f, -0.5f, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f), Vec2f(0.0f, 0.0f)},
-        {Vec3f(0.0f, 0.5f, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f), Vec2f(0.0f, 1.0f)},
-        {Vec3f(0.5f, -0.5f, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f), Vec2f(1.0f, 0.0f)},
+    const D3D12_INPUT_CLASSIFICATION vertex = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+    static const std::vector<D3D12_INPUT_ELEMENT_DESC> layout = {
+        {"POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, vertex, 0},
+        {"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 16, vertex, 0},
     };
 
+    struct A2V
+    {
+      Vec4f pos;
+      Vec4f col;
+    };
+
+    const A2V tri[9] = {
+        {Vec4f(-0.5f, -0.5f, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f)},
+        {Vec4f(0.0f, 0.5f, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f)},
+        {Vec4f(0.5f, -0.5f, 0.0f), Vec4f(0.0f, 1.0f, 0.0f, 1.0f)},
+    };
+
+    float checkdata[1024] = {};
+    checkdata[64 + 7] = 1.234f;
+
     ID3D12ResourcePtr vb = MakeBuffer().Data(tri);
+    ID3D12ResourcePtr cbv = MakeBuffer().Data(checkdata);
+    ID3D12ResourcePtr srv = MakeBuffer().Data(checkdata);
+    ID3D12ResourcePtr uav = MakeBuffer().UAV().Data(checkdata);
 
-    ID3D12RootSignaturePtr sig = MakeSig({});
+    ID3D12RootSignaturePtr patchsig = MakeSig({
+        cbvParam(D3D12_SHADER_VISIBILITY_VERTEX, 0, 0),
+        srvParam(D3D12_SHADER_VISIBILITY_VERTEX, 0, 0),
+        uavParam(D3D12_SHADER_VISIBILITY_VERTEX, 0, 0),
+    });
 
-    ID3D12CommandSignaturePtr cmdsig = MakeCommandSig(NULL, {vbArg(0), drawArg()});
+    ID3D12CommandSignaturePtr patchArgSig =
+        MakeCommandSig(patchsig, {vbArg(0), cbvArg(0), srvArg(1), uavArg(2), drawArg()});
 
     struct
     {
       D3D12_VERTEX_BUFFER_VIEW vb;
+      D3D12_GPU_VIRTUAL_ADDRESS cbv;
+      D3D12_GPU_VIRTUAL_ADDRESS srv;
+      D3D12_GPU_VIRTUAL_ADDRESS uav;
       D3D12_DRAW_ARGUMENTS draw;
-    } args;
+    } patchargs;
 
-    args.vb.BufferLocation = vb->GetGPUVirtualAddress();
-    args.vb.SizeInBytes = sizeof(tri);
-    args.vb.StrideInBytes = sizeof(DefaultA2V);
-    args.draw.VertexCountPerInstance = 3;
-    args.draw.InstanceCount = 1;
-    args.draw.StartInstanceLocation = 0;
-    args.draw.StartVertexLocation = 0;
+    patchargs.vb.BufferLocation = vb->GetGPUVirtualAddress();
+    patchargs.vb.SizeInBytes = sizeof(tri);
+    patchargs.vb.StrideInBytes = sizeof(A2V);
+    patchargs.cbv = cbv->GetGPUVirtualAddress() + 256;
+    patchargs.srv = srv->GetGPUVirtualAddress() + 256;
+    patchargs.uav = uav->GetGPUVirtualAddress() + 256;
+    patchargs.draw.VertexCountPerInstance = 3;
+    patchargs.draw.InstanceCount = 1;
+    patchargs.draw.StartInstanceLocation = 0;
+    patchargs.draw.StartVertexLocation = 0;
 
-    ID3D12ResourcePtr argBuf = MakeBuffer().Upload().Size(sizeof(args)).Data(&args);
+    ID3D12ResourcePtr patchArgBuf = MakeBuffer().Upload().Size(sizeof(patchargs)).Data(&patchargs);
 
-    ID3D12PipelineStatePtr pso = MakePSO().RootSig(sig).InputLayout().VS(vsblob).PS(psblob);
+    ID3D12PipelineStatePtr patchpso =
+        MakePSO().RootSig(patchsig).InputLayout(layout).VS(vsblob).PS(psblob);
+
+    ID3D12CommandSignaturePtr compArgSig = MakeCommandSig(NULL, {dispatchArg()});
+
+    D3D12_DISPATCH_ARGUMENTS compargs;
+
+    compargs.ThreadGroupCountX = 12;
+    compargs.ThreadGroupCountY = 30;
+    compargs.ThreadGroupCountZ = 10;
+
+    ID3D12ResourcePtr compArgBuf = MakeBuffer().Upload().Size(sizeof(compargs)).Data(&compargs);
+
+    ID3D12ResourcePtr compuav = MakeBuffer().UAV().Size(1024 * 1024 * 4);
+
+    MakeUAV(compuav)
+        .Format(DXGI_FORMAT_R32G32B32A32_UINT)
+        .FirstElement(4096)
+        .NumElements(256000)
+        .CreateGPU(1);
+
+    ID3D12RootSignaturePtr compsig = MakeSig({
+        tableParam(D3D12_SHADER_VISIBILITY_ALL, D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0, 0, 1, 0),
+        tableParam(D3D12_SHADER_VISIBILITY_ALL, D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0, 1, 1, 1),
+    });
+
+    ID3DBlobPtr csblob = Compile(comp, "main", "cs_5_0");
+
+    ID3D12PipelineStatePtr comppso = MakePSO().RootSig(compsig).CS(csblob);
+
+    ID3D12ResourcePtr customvbargs = MakeBuffer().Size(1024 * 1024 * 4);
+
+    ID3D12RootSignaturePtr plainsig = MakeSig({});
+    ID3D12PipelineStatePtr plainpso =
+        MakePSO().RootSig(plainsig).InputLayout(layout).VS(vs2blob).PS(psblob);
+
+    ID3D12CommandSignaturePtr plainArgSig = MakeCommandSig(NULL, {drawArg()});
 
     ResourceBarrier(vb, D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+    ResourceBarrier(
+        customvbargs, D3D12_RESOURCE_STATE_COMMON,
+        D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER | D3D12_RESOURCE_STATE_INDIRECT_ARGUMENT);
 
     while(Running())
     {
-      ID3D12GraphicsCommandListPtr cmd = GetCommandBuffer();
+      std::vector<ID3D12GraphicsCommandListPtr> cmds;
 
+      ID3D12GraphicsCommandListPtr cmd = GetCommandBuffer();
       Reset(cmd);
+      cmds.push_back(cmd);
+
+      uint32_t zero[4] = {};
+      cmd->ClearUnorderedAccessViewUint(
+          MakeUAV(compuav).Format(DXGI_FORMAT_R32G32B32A32_UINT).CreateGPU(0),
+          MakeUAV(compuav).Format(DXGI_FORMAT_R32G32B32A32_UINT).CreateClearCPU(0), compuav, zero,
+          0, NULL);
 
       ID3D12ResourcePtr bb = StartUsingBackbuffer(cmd, D3D12_RESOURCE_STATE_RENDER_TARGET);
 
       D3D12_CPU_DESCRIPTOR_HANDLE rtv =
           MakeRTV(bb).Format(DXGI_FORMAT_R8G8B8A8_UNORM_SRGB).CreateCPU(0);
 
-      for(int i = 0; i < 8; ++i)
+      pushMarker(cmd, "Multiple draws");
+      for(int i = 0; i < 8; i++)
       {
-        ClearRenderTargetView(cmd, rtv, {1.0f, 0.0f, 0.0f, 1.0f});
+        ClearRenderTargetView(cmd, rtv, {0.0f, 0.0f, 0.0f, 1.0f});
 
         cmd->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-        IASetVertexBuffer(cmd, vb, sizeof(DefaultA2V), 0);
-        cmd->SetPipelineState(pso);
-        cmd->SetGraphicsRootSignature(sig);
+        cmd->SetPipelineState(patchpso);
+        cmd->SetGraphicsRootSignature(patchsig);
 
         RSSetViewport(cmd, {0.0f, 0.0f, (float)screenWidth, (float)screenHeight, 0.0f, 1.0f});
         RSSetScissorRect(cmd, {0, 0, screenWidth, screenHeight});
 
         OMSetRenderTargets(cmd, {rtv}, {});
 
-        cmd->ExecuteIndirect(cmdsig, 1, argBuf, 0, NULL, 0);
+        cmd->ExecuteIndirect(patchArgSig, 1, patchArgBuf, 0, NULL, 0);
       }
+      popMarker(cmd);
+
+      setMarker(cmd, "Post draw");
+
+      cmd->Close();
+      cmd = GetCommandBuffer();
+      Reset(cmd);
+      cmds.push_back(cmd);
+
+      setMarker(cmd, "Separate Post draw");
+
+      pushMarker(cmd, "Single dispatch");
+      {
+        cmd->SetPipelineState(comppso);
+        cmd->SetComputeRootSignature(compsig);
+
+        cmd->SetDescriptorHeaps(1, &m_CBVUAVSRV.GetInterfacePtr());
+
+        cmd->SetComputeRootDescriptorTable(0, m_CBVUAVSRV->GetGPUDescriptorHandleForHeapStart());
+        cmd->SetComputeRootDescriptorTable(1, m_CBVUAVSRV->GetGPUDescriptorHandleForHeapStart());
+
+        cmd->ExecuteIndirect(compArgSig, 1, compArgBuf, 0, NULL, 0);
+      }
+      popMarker(cmd);
+
+      setMarker(cmd, "Post Single dispatch");
+
+      cmd->Close();
+
+      cmd = GetCommandBuffer();
+      Reset(cmd);
+      cmds.push_back(cmd);
+
+      setMarker(cmd, "Separate Post Single dispatch");
+
+      D3D12_RESOURCE_BARRIER barrier;
+      barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV;
+      barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+      barrier.UAV.pResource = compuav;
+      cmd->ResourceBarrier(1, &barrier);
+
+      ResourceBarrier(cmd, compuav, D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
+                      D3D12_RESOURCE_STATE_COPY_SOURCE);
+
+      cmd->CopyBufferRegion(customvbargs, 0, compuav, 0, 4 * 1024 * 1024);
+
+      ResourceBarrier(
+          cmd, customvbargs, D3D12_RESOURCE_STATE_COPY_DEST,
+          D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER | D3D12_RESOURCE_STATE_INDIRECT_ARGUMENT);
+
+      cmd->Close();
+
+      cmd = GetCommandBuffer();
+      Reset(cmd);
+      cmds.push_back(cmd);
+
+      pushMarker(cmd, "Custom order draw");
+      {
+        cmd->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+        D3D12_VERTEX_BUFFER_VIEW view;
+        view.BufferLocation = customvbargs->GetGPUVirtualAddress() + 4096 * (sizeof(Vec4f)) + 256;
+        view.StrideInBytes = sizeof(A2V);
+        view.SizeInBytes = sizeof(A2V) * 1120;
+        cmd->IASetVertexBuffers(0, 1, &view);
+
+        cmd->SetPipelineState(plainpso);
+        cmd->SetGraphicsRootSignature(plainsig);
+
+        RSSetViewport(cmd, {0.0f, 0.0f, (float)screenWidth, (float)screenHeight, 0.0f, 1.0f});
+        RSSetScissorRect(cmd, {0, 0, screenWidth, screenHeight});
+
+        OMSetRenderTargets(cmd, {rtv}, {});
+
+        cmd->ExecuteIndirect(plainArgSig, 8, customvbargs, 4096 * (sizeof(Vec4f)) + sizeof(Vec4u),
+                             NULL, 0);
+      }
+      popMarker(cmd);
 
       FinishUsingBackbuffer(cmd, D3D12_RESOURCE_STATE_RENDER_TARGET);
 
       cmd->Close();
 
-      Submit({cmd});
+      Submit(cmds);
 
       Present();
     }
--- util/test/tests/D3D12/D3D12_Execute_Indirect.py
+++ util/test/tests/D3D12/D3D12_Execute_Indirect.py
@@ -1,42 +1,175 @@
 import renderdoc as rd
 import rdtest
+import struct
 
 
 class D3D12_Execute_Indirect(rdtest.TestCase):
     demos_test_name = 'D3D12_Execute_Indirect'
 
     def check_capture(self):
-        from_eid = 0
+        from_eid = self.find_action("Multiple draws").eventId
         for i in range(8):
             action = self.find_action("IndirectDraw", from_eid)
             self.controller.SetFrameEvent(action.eventId, False)
-            # Should be a green triangle in the centre of the screen on a red background
-            self.check_triangle(back=[1.0, 0.0, 0.0, 1.0])
-            postvs_data = self.get_postvs(action, rd.MeshDataStage.VSOut, 0, action.numIndices)
 
-            postvs_ref = {
-            0: {
-            'vtx': 0,
-            'idx': 0,
-            'SV_POSITION': [-0.5, -0.5, 0.0, 1.0],
-            'COLOR': [0.0, 1.0, 0.0, 1.0],
-            'TEXCOORD': [0.0, 0.0],
-            },
-            1: {
-            'vtx': 1,
-            'idx': 1,
-            'SV_POSITION': [0.0, 0.5, 0.0, 1.0],
-            'COLOR': [0.0, 1.0, 0.0, 1.0],
-            'TEXCOORD': [0.0, 1.0],
-            },
-            2: {
-            'vtx': 2,
-            'idx': 2,
-            'SV_POSITION': [0.5, -0.5, 0.0, 1.0],
-            'COLOR': [0.0, 1.0, 0.0, 1.0],
-            'TEXCOORD': [1.0, 0.0],
-            },
+            self.check_triangle(back=[0.0, 0.0, 0.0, 1.0])
+
+            # Should be a green triangle in the centre of the screen on a black background
+            self.check_triangle(back=[0.0, 0.0, 0.0, 1.0])
+            vsin_ref = {
+                0: {
+                    'vtx': 0,
+                    'idx': 0,
+                    'POSITION': [-0.5, -0.5, 0.0, 0.0],
+                    'COLOR': [0.0, 1.0, 0.0, 1.0],
+                },
+                1: {
+                    'vtx': 1,
+                    'idx': 1,
+                    'POSITION': [0.0, 0.5, 0.0, 0.0],
+                    'COLOR': [0.0, 1.0, 0.0, 1.0],
+                },
+                2: {
+                    'vtx': 2,
+                    'idx': 2,
+                    'POSITION': [0.5, -0.5, 0.0, 0.0],
+                    'COLOR': [0.0, 1.0, 0.0, 1.0],
+                },
             }
-
+            self.check_mesh_data(vsin_ref, self.get_vsin(action))
+            postvs_data = self.get_postvs(action, rd.MeshDataStage.VSOut, 0, action.numIndices)
+            postvs_ref = {
+                0: {
+                    'vtx': 0,
+                    'idx': 0,
+                    'SV_POSITION': [-0.5, -0.5, 0.0, 1.0],
+                    'COLOR': [0.0, 1.0, 0.0, 1.0],
+                },
+                1: {
+                    'vtx': 1,
+                    'idx': 1,
+                    'SV_POSITION': [0.0, 0.5, 0.0, 1.0],
+                    'COLOR': [0.0, 1.0, 0.0, 1.0],
+                },
+                2: {
+                    'vtx': 2,
+                    'idx': 2,
+                    'SV_POSITION': [0.5, -0.5, 0.0, 1.0],
+                    'COLOR': [0.0, 1.0, 0.0, 1.0],
+                },
+            }
             self.check_mesh_data(postvs_ref, postvs_data)
+
             from_eid = action.eventId + 1
+
+            pipe = self.controller.GetPipelineState()
+
+            vbs = pipe.GetVBuffers()
+            ro = pipe.GetReadOnlyResources(rd.ShaderStage.Vertex)
+            rw = pipe.GetReadWriteResources(rd.ShaderStage.Vertex)
+            self.check(vbs[0].resourceId != rd.ResourceId())
+            self.check(ro[0].resources[0].resourceId != rd.ResourceId())
+            self.check(rw[0].resources[0].resourceId != rd.ResourceId())
+            self.check(pipe.GetConstantBuffer(rd.ShaderStage.Vertex, 0, 0).resourceId != rd.ResourceId())
+
+        action = self.find_action("Post draw")
+        self.controller.SetFrameEvent(action.eventId, False)
+
+        # triangle should still be visible
+        self.check_triangle(back=[0.0, 0.0, 0.0, 1.0])
+
+        # but state should be reset
+        pipe = self.controller.GetPipelineState()
+
+        vbs = pipe.GetVBuffers()
+        ro = pipe.GetReadOnlyResources(rd.ShaderStage.Vertex)
+        rw = pipe.GetReadWriteResources(rd.ShaderStage.Vertex)
+        self.check(len(vbs) == 0 or vbs[0].resourceId == rd.ResourceId())
+        self.check(len(ro) == 0 or ro[0].resources[0].resourceId == rd.ResourceId())
+        self.check(len(rw) == 0 or rw[0].resources[0].resourceId == rd.ResourceId())
+        self.check(pipe.GetConstantBuffer(rd.ShaderStage.Vertex, 0, 0).resourceId == rd.ResourceId())
+
+        rdtest.log.success("State is reset after execute")
+
+        action = self.find_action("Post Single dispatch")
+        self.controller.SetFrameEvent(action.eventId, False)
+
+        pipe = self.controller.GetPipelineState()
+        rw = pipe.GetReadWriteResources(rd.ShaderStage.Compute)
+
+        for z in range(10):
+            for y in range(30):
+                for x in range(12):
+                    idx = z*30*12+y*12+x
+                    value = struct.unpack_from('4f', self.controller.GetBufferData(rw[0].resources[0].resourceId, 16*idx, 16))
+                    expect = [float(x), float(y), float(z), float(idx)]
+
+                    if not rdtest.value_compare(expect, value):
+                        raise rdtest.TestFailureException(
+                            "buffer at {},{},{}: {} doesn't match expected {}".format(x, y, z, value, expect))
+
+        rdtest.log.success("Dispatch buffer output is correct")
+
+        # The final draw is in indeterminate order because the parameters are defined by a compute shader in
+        # indeterminate order
+        # If the vertex buffer is referenced in the wrong order (e.g. by cached draw parameters) it will show exploding
+        # polygons. To check the behaviour we can't require any given draw appear in any given place. However each
+        # time we replay it should be self-consistent - after selecting the 4th draw then eactly 4 draws should appear.
+        # And of course no exploding polys!
+
+        action = self.find_action("Custom order draw")
+        action = self.find_action("IndirectDraw", action.eventId)
+
+        drawPoints = [
+            (60, 20),
+            (210, 20),
+            (360, 20),
+
+            (60, 130),
+            (360, 130),
+
+            (60, 240),
+            (210, 240),
+            (360, 240),
+        ]
+
+        sdfile = self.controller.GetStructuredFile()
+
+        # do N passes since it will be unpredictable
+        for passNum in range(50):
+            for drawNum in range(8):
+                self.controller.SetFrameEvent(action.eventId + drawNum, False)
+
+                pipe = self.controller.GetPipelineState()
+                out = pipe.GetOutputTargets()[0].resourceId
+
+                count = 0
+                draws = []
+                for i, p in enumerate(drawPoints):
+                    picked = self.controller.PickPixel(out, p[0], p[1], rd.Subresource(), rd.CompType.UNorm)
+                    if rdtest.value_compare(picked.floatValue, [0.0, 1.0, 0.0, 1.0]):
+                        count += 1
+                        draws += [i]
+
+                if not rdtest.value_compare(drawNum + 1, count):
+                    raise rdtest.TestFailureException(
+                        "With {} selected we should have {} draws, but counted {} draws".format(action.GetName(sdfile),
+                                                                                                drawNum + 1, count))
+
+                rdtest.log.print("With draw #{} selected we saw draws {} active".format(drawNum, str(draws)))
+
+                # the exploded verts are calibrated to render as purple. We don't handle the case where exploding polys
+                # reference vertices from other draws, but this _should_ not happen as we leave a large margin between
+                # each draw's segments
+
+                data = self.controller.GetTextureData(out, rd.Subresource(0, 0, 0))
+                tex = self.get_texture(out)
+                rdtest.log.print("{} - {} {} ".format(len(data), tex.width, tex.height))
+                pixels = [struct.unpack_from("4B", data, 4 * p) for p in range(int(tex.width * tex.height))]
+                unique_pixels = list(set(pixels))
+
+                if (255, 0, 255, 255) in unique_pixels:
+                    raise rdtest.TestFailureException(
+                        "Detected an exploded polygon with {} selected".format(action.GetName(sdfile)))
+
+            rdtest.log.success("Pass {} of unordered draw was correct")
