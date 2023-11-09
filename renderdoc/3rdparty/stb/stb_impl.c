bcb7dfd3aebe32c669b5122f00ba27dd12ea54ca
jathoma98 <34799315+jathoma98@users.noreply.github.com>
2023-11-09 18:22:14 -0500

    Update stb_image version 2.12 -> 2.28
    stb_image v2.12 has a memory corruption bug, updated to v2.28 to fix.
    
    The only real API change is stb_image_resize.h is now stb_image_resize2.h, and stbir_resize_uint8_srgb() doesnt take alpha channels or flags as params.

--- renderdoc/3rdparty/stb/stb_impl.c
+++ renderdoc/3rdparty/stb/stb_impl.c
@@ -17,8 +17,9 @@
 #pragma warning(disable:4189) // local variable is initialized but not referenced
 #pragma warning(disable:4244) // <narrowing conversion>, possible loss of data
 #pragma warning(disable:4702) // unreachable code
+#pragma warning(disable:4204) // nonstandard extension used: non-constant aggregate initializer 
 
 #include "stb_image.h"
 #include "stb_image_write.h"
-#include "stb_image_resize.h"
+#include "stb_image_resize2.h"
 #include "stb_truetype.h"
