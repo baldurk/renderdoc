98dd8062986e89d96ae15dbca4d8f676f41f2504
baldurk <baldurk@baldurk.org>
2023-11-17 01:59:18 +0000

    Remove duplicate docstring

--- renderdoc/api/replay/replay_enums.h
+++ renderdoc/api/replay/replay_enums.h
@@ -4495,10 +4495,6 @@ DOCUMENT(R"(A set of flags for ``ShaderStage`` stages
 
   The flag for :data:`ShaderStage.Compute`.
 
-.. data:: Compute
-
-  The flag for :data:`ShaderStage.Compute`.
-
 .. data:: Task
 
   The flag for :data:`ShaderStage.Task`.
