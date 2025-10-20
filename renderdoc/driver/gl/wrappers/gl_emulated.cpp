8607a59ab1dded32d99c2adfd4d28bdaca1a7dce
Martyn Jacques <martyn.jacques@arm.com>
2025-10-20 16:42:06 +0100

    Fix component sizing for GL texture copies

--- renderdoc/driver/gl/wrappers/gl_emulated.cpp
+++ renderdoc/driver/gl/wrappers/gl_emulated.cpp
@@ -3499,7 +3499,7 @@ void APIENTRY _glGetTexImage(GLenum target, GLint level, const GLenum format, co
       size_t readCompSize = GetByteSize(1, 1, 1, eGL_RED, readType);
 
       if(depthFormat)
-        readCompSize = GetByteSize(1, 1, 1, readFormat, readType);
+        readCompSize = GetByteSize(1, 1, 1, readFormat, readType) / readCompCount;
 
       // if the type didn't change from what the caller expects, we only changed the number of
       // components. This is easy to remap
