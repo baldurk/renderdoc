755ab657ef06cedcbfbf8fb5ac7c4a9d1a6e6f4c
baldurk <baldurk@baldurk.org>
2026-09-18 11:37:06 +0100

    Pass visible range through to custom shaders consistently

--- renderdoc/driver/vulkan/vk_replay.cpp
+++ renderdoc/driver/vulkan/vk_replay.cpp
@@ -5193,8 +5193,8 @@ ResourceId VulkanReplay::ApplyCustomShader(TextureDisplay &display)
   disp.linearDisplayAsGamma = false;
   disp.subresource = display.subresource;
   disp.overlay = DebugOverlay::NoOverlay;
-  disp.rangeMin = 0.0f;
-  disp.rangeMax = 1.0f;
+  disp.rangeMin = display.rangeMin;
+  disp.rangeMax = display.rangeMax;
   disp.rawOutput = false;
   disp.scale = 1.0f;
 
